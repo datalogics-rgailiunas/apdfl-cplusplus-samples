a196a8bd3c211540ee396a8ea0aa06ebef664501
Jeremy Savarese <jsavarese@datalogics.com>
2015-07-02 15:25:57 -0500

    Tabs are now spaces

--- AddDocumentInformation/AddDocumentInformation.cpp
+++ AddDocumentInformation/AddDocumentInformation.cpp
@@ -65,53 +65,53 @@
 
 int main()
 {
-	//Initialize the APDFL prior to making any APDFL calls
-	ASErrorCode errorCode = MyPDFLInit();
+    //Initialize the APDFL prior to making any APDFL calls
+    ASErrorCode errorCode = MyPDFLInit();
 
-	//Check for errors upon initialization of APDFL
-	if (errorCode != 0)
-	{
-		std::cerr << "Initialization error. See \"AcroErr.h\" for more info.\n" << std::endl;
-		std::cerr << "Error system: " << ErrGetSystem(errorCode) << std::endl;
-		std::cerr << "Error Severity: " << ErrGetSeverity(errorCode) << std::endl;
-		std::cerr << "Error Code: " << ErrGetCode(errorCode) << std::endl;
+    //Check for errors upon initialization of APDFL
+    if (errorCode != 0)
+    {
+        std::cerr << "Initialization error. See \"AcroErr.h\" for more info.\n" << std::endl;
+        std::cerr << "Error system: " << ErrGetSystem(errorCode) << std::endl;
+        std::cerr << "Error Severity: " << ErrGetSeverity(errorCode) << std::endl;
+        std::cerr << "Error Code: " << ErrGetCode(errorCode) << std::endl;
 
-		return errorCode;
-	}
+        return errorCode;
+    }
 
-	DURING
+    DURING
 //*************************************************************************
 //Step 1) Open the PDF Document
 //*************************************************************************
 
-		//File path used to open the PDF Document
-		wchar_t * inputFilePath = L"../Input/AddDocumentInformation.pdf";
+        //File path used to open the PDF Document
+        wchar_t * inputFilePath = L"../Input/AddDocumentInformation.pdf";
 
-		//Argument passed to function ASTextFromUnicode
-		ASUnicodeFormat unicodeFormat;
+        //Argument passed to function ASTextFromUnicode
+        ASUnicodeFormat unicodeFormat;
 
-		//Determine host computers unicode format	
-		if (sizeof(wchar_t) == 2)
-			unicodeFormat = kUTF16HostEndian;
-		else
-			unicodeFormat = kUTF32HostEndian;
+        //Determine host computers unicode format	
+        if (sizeof(wchar_t) == 2)
+            unicodeFormat = kUTF16HostEndian;
+        else
+            unicodeFormat = kUTF32HostEndian;
 
-		//ASText is the unicode string used to create ASPathName for opening the PDDoc
-		ASText asText = ASTextFromUnicode(reinterpret_cast<ASUTF16Val*>(inputFilePath), unicodeFormat);
+        //ASText is the unicode string used to create ASPathName for opening the PDDoc
+        ASText asText = ASTextFromUnicode(reinterpret_cast<ASUTF16Val*>(inputFilePath), unicodeFormat);
 
-		//Create the asPathName to open the input file
-		ASPathName asPathName = ASFileSysCreatePathFromDIPathText(NULL, asText, NULL);
+        //Create the asPathName to open the input file
+        ASPathName asPathName = ASFileSysCreatePathFromDIPathText(NULL, asText, NULL);
 
-		//Free up resources
-		ASTextDestroy(asText);
-		
-		//Open the Document
-		PDDoc pdDoc = PDDocOpen(asPathName, NULL, NULL, true);
+        //Free up resources
+        ASTextDestroy(asText);
+        
+        //Open the Document
+        PDDoc pdDoc = PDDocOpen(asPathName, NULL, NULL, true);
 
-		std::wcout << L"Document was sucessfully opened." << std::endl;
+        std::wcout << L"Document was sucessfully opened." << std::endl;
 
-		//Free up resources
-		ASFileSysReleasePath(NULL, asPathName);
+        //Free up resources
+        ASFileSysReleasePath(NULL, asPathName);
 
 //*************************************************************************
 //Step 2) Insert document information into Document
@@ -124,65 +124,65 @@ int main()
 //        "value" is the data being inserted
 //*************************************************************************
 
-		//Create unicode strings for inserting the document's Title into the document.
-		ASText key = ASTextFromUnicode(reinterpret_cast<ASUTF16Val*>(L"Title"), unicodeFormat);
-		ASText value = ASTextFromUnicode(reinterpret_cast<ASUTF16Val*>(L"Sample Title"), unicodeFormat);
+        //Create unicode strings for inserting the document's Title into the document.
+        ASText key = ASTextFromUnicode(reinterpret_cast<ASUTF16Val*>(L"Title"), unicodeFormat);
+        ASText value = ASTextFromUnicode(reinterpret_cast<ASUTF16Val*>(L"Sample Title"), unicodeFormat);
 
-		//Insert the document information
-		PDDocSetInfoAsASText(pdDoc, key, value);
+        //Insert the document information
+        PDDocSetInfoAsASText(pdDoc, key, value);
 
-		//Free up the resources
-		ASTextDestroy(key);
-		ASTextDestroy(value);
+        //Free up the resources
+        ASTextDestroy(key);
+        ASTextDestroy(value);
 
-		std::wcout << L"The documents title was inserted." << std::endl;
+        std::wcout << L"The documents title was inserted." << std::endl;
 
-		//Create unicode strings for inserting the document's Author into the document.
-		key = ASTextFromUnicode(reinterpret_cast<ASUTF16Val*>(L"Author"), unicodeFormat);
-		value = ASTextFromUnicode(reinterpret_cast<ASUTF16Val*>(L"Sample Author"), unicodeFormat);
+        //Create unicode strings for inserting the document's Author into the document.
+        key = ASTextFromUnicode(reinterpret_cast<ASUTF16Val*>(L"Author"), unicodeFormat);
+        value = ASTextFromUnicode(reinterpret_cast<ASUTF16Val*>(L"Sample Author"), unicodeFormat);
 
-		//Insert the document information
-		PDDocSetInfoAsASText(pdDoc, key, value);
+        //Insert the document information
+        PDDocSetInfoAsASText(pdDoc, key, value);
 
-		//Free up the resources
-		ASTextDestroy(key);
-		ASTextDestroy(value);
+        //Free up the resources
+        ASTextDestroy(key);
+        ASTextDestroy(value);
 
-		std::wcout << L"The documents author was inserted." << std::endl;
+        std::wcout << L"The documents author was inserted." << std::endl;
 
 //*************************************************************************
 //Step 3) Save the Document and release Resources
 //*************************************************************************
 
-		//Create unicode string for output file
-		asText = ASTextFromUnicode(reinterpret_cast<ASUTF16Val*>(L"out.pdf"), unicodeFormat);
+        //Create unicode string for output file
+        asText = ASTextFromUnicode(reinterpret_cast<ASUTF16Val*>(L"out.pdf"), unicodeFormat);
 
-		//Create the asPathName to open the input file
-		asPathName = ASFileSysCreatePathFromDIPathText(NULL, asText, NULL);
+        //Create the asPathName to open the input file
+        asPathName = ASFileSysCreatePathFromDIPathText(NULL, asText, NULL);
 
-		//Save the new document
-		PDDocSave(pdDoc, PDSaveFull, asPathName, NULL, NULL, NULL);
+        //Save the new document
+        PDDocSave(pdDoc, PDSaveFull, asPathName, NULL, NULL, NULL);
 
-		std::wcout << L"The PDF Document has been saved and now contains Document Information." << std::endl;
+        std::wcout << L"The PDF Document has been saved and now contains Document Information." << std::endl;
 
-		//Release remaining resources
-		ASTextDestroy(asText);
-		ASFileSysReleasePath(NULL, asPathName);
-		PDDocClose(pdDoc);
+        //Release remaining resources
+        ASTextDestroy(asText);
+        ASFileSysReleasePath(NULL, asPathName);
+        PDDocClose(pdDoc);
 
-		std::wcout << L"All remaining resources have been released" << std::endl;
+        std::wcout << L"All remaining resources have been released" << std::endl;
 
-	HANDLER
-		//If there was an exception generate an error code 
-		errorCode = ERRORCODE;
+    HANDLER
+        //If there was an exception generate an error code 
+        errorCode = ERRORCODE;
 
-		char buf[256];
+        char buf[256];
 
-		ASGetErrorString(ERRORCODE, buf, sizeof(buf));
+        ASGetErrorString(ERRORCODE, buf, sizeof(buf));
 
-		//Print out error code
-		std::cerr << "Error Code: " << errorCode << "Error Message: " << buf << std::endl;
-	END_HANDLER
+        //Print out error code
+        std::cerr << "Error Code: " << errorCode << "Error Message: " << buf << std::endl;
+    END_HANDLER
 
-	return errorCode;
+    return errorCode;
 }
