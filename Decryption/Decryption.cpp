a243f356d4253efc8828506b6175f5125d2d5f85
Jordan Marks <jmarks@datalogics.com>
2015-07-02 15:15:09 -0500

    Conforms to style conventions. * Not a single tab occurs anywhere. You have my word and my honor. * Brace style now disgusting, as per conventions ;P * OpenAuthProc and password now in the same source as main. * Comments/formatting cleaned up. * By the way, I didnt' change anything about sampleutils.cpp or .h,   because those files will be removed later and consolidated with Common   files to be pushed soon...

--- Decryption/Decryption.cpp
+++ Decryption/Decryption.cpp
@@ -1,8 +1,18 @@
 // Copyright (c) 2015, Datalogics, Inc. All rights reserved.
+
+//************************************************************************
+// Sample: Decryption - Removes security from a document
 //
-// Sample Decryption / Opens a password-protected document, removes the password, saves, 
-//					   and ensures the document is no longer password-protected.
+// This sample completely removes the security from a password-
+// protected document.
 //
+//Steps:
+// 1) Open the document with the password
+// 2) Remove the encryption
+// 3) Save and close the document
+// 4) Try opening it again to ensure the encryption is gone
+//************************************************************************
+
 // This agreement is between Datalogics, Inc. 101 N. Wacker Drive, Suite 1800,
 // Chicago, IL 60606 ("Datalogics") and you, an end user who downloads
 // source code examples for integrating to the Adobe PDF Library
@@ -48,16 +58,22 @@
 // NEITHER DATALOGICS WARRANT AGAINST ANY BUG, ERROR, OMISSION, DEFECT,
 // DEFICIENCY, OR NONCONFORMITY IN ANY EXAMPLE CODE.
 
-//Sample includes
-#include "OpenAuthorizationProcedure.cpp"
-
-//STD includes
+#include "SampleUtils.h"
 #include <iostream>
 
-int main(){
+static char* password = "";     //The document's password is stored statically
+                                //because OpenAuthProc is static.
+                                //Note that the password cannot be a wchar_t*.
+//Called by PDDocOpenEx to obtain permission to open the document
+//by supplying the password.
+static ACCB1 ASBool ACCB2 openAuthorizationProcedure(PDDoc encrypted, void *clientData);
+
+int main()
+{
 	//Initialize the APDF libary
     int initError = MyPDFLInit();
-    if (initError) {
+    if (initError)
+    {
         std::wcerr << L"Initialization error. See \"AcroErr.h\" for more info.\n" << std::endl;
         std::wcerr << L"Error system: " << ErrGetSystem(initError) << std::endl;
         std::wcerr << L"Error Severity: " << ErrGetSeverity(initError) << std::endl;
@@ -65,74 +81,138 @@ int main(){
         return initError;
     }
 
-	//APDFL variables
-	ASErrorCode errCode = 0; //Tracks errors
+    //APDFL variables
+    ASErrorCode errCode = 0; //Tracks errors
 
-	//Sample variables
-	//Decryptor decryptor;                               //Handles most of the sample code
+    //Sample variables
     Utilities util;                                      //Performs some common actions
-	PDDoc encrypted_document;                            //For processing the still-encrypted document
+    PDDoc encrypted_document;                            //For processing the still-encrypted document
     PDDoc unencrypted_document;                          //For checking that the document has been unencrypted.
-    ASPathName encrypted_path = util.makeASPathName(L"../Input/encrypted.pdf"); //Filepath of the encrypted document
-	char* myPassword = "mypassword";                     //Password for that document. NOTE: This does not work for wide chars. 
+    ASPathName encrypted_path;                           //Filepath of the encrypted document
 
-	DURING
 
-		//Open the encrypted PDF document
-        setPassword(myPassword);
-        encrypted_document = PDDocOpenEx(encrypted_path, ASGetDefaultFileSys(),
-			ASCallbackCreateProto(PDAuthProcEx, &openAuthorizationProcedure), 0, true);
-		std::wcout << L"Opened the document." << std::endl;
+    DURING
 
-		//Remove the encryption
-		PDDocSetNewCryptHandler(encrypted_document, ASAtomNull); //Completely removes security from the document
-		std::wcout << L"The encryption was removed." << std::endl;
+    //==================================================================
+    //Step 1) Open the document with the password
+    //==================================================================
 
-		//Overwrite the document
-		PDDocSave(encrypted_document,        //Document to save
-			PDDocNeedsSave | PDDocIsOpen,    //PDDocSaveFlags
-            encrypted_path,                  //ASPath to save
-			ASGetDefaultFileSys(),           //File system
-			NULL, NULL);                     //Progress monitor, progreess monitor client data
-		std::wcout << L"The document was saved..." << std::endl;
+    password = "mypassword";
+	encrypted_path = util.makeASPathName(L"../Input/encrypted.pdf");
+    encrypted_document = PDDocOpenEx(encrypted_path, ASGetDefaultFileSys(), //Calls openAuthorizationProcedure to supply the password
+                            ASCallbackCreateProto(PDAuthProcEx, &openAuthorizationProcedure), 0, true);
+							
+    std::wcout << L"Opened the document." << std::endl;
 
-		//Close the document
-		PDDocClose(encrypted_document);
-		
-		//Verify it is no longer encrypted by attempting to open it
-        ASErrorCode openError = 0;
-	    DURING
-            unencrypted_document = PDDocOpen(encrypted_path, NULL, NULL, true);
-	    HANDLER
-            openError = ERRORCODE;
-	    END_HANDLER
-		if (openError){
-		    if (ErrGetSystem(openError) == ErrSysPDDoc && ErrGetCode(openError) == pdErrNeedPassword){
-		    	std::wcout << L"...But still requires a password [FAILURE]." << std::endl;
-		    } else{
-		    	std::wcout << L"An unexpected error occured." << std::endl;
-		    	ASRaise(openError);
-		    }
-		} else{
-			std::wcout << L"...And is no longer encrypted!" << std::endl;
+    //==================================================================
+    //Step 2) Remove the encryption
+    //==================================================================
+
+    PDDocSetNewCryptHandler(encrypted_document, ASAtomNull); //Completely removes security from the document
+	
+    std::wcout << L"The encryption was removed." << std::endl;
+
+    //==================================================================
+    //Step 2) Save and close the document
+    //==================================================================
+
+    PDDocSave(encrypted_document,PDDocNeedsSave | PDDocIsOpen,
+        encrypted_path,ASGetDefaultFileSys(),NULL, NULL);
+
+    std::wcout << L"The document was saved..." << std::endl;
+
+    PDDocClose(encrypted_document);
+
+    //==================================================================
+    //Step 2) Try opening it again to ensure the encryption is gone
+    //==================================================================
+
+    ASErrorCode openError = 0;
+
+    DURING
+        unencrypted_document = PDDocOpen(encrypted_path, NULL, NULL, true);
+    HANDLER
+        openError = ERRORCODE;
+    END_HANDLER
+        if (openError)
+        {
+            if (ErrGetSystem(openError) == ErrSysPDDoc && ErrGetCode(openError) == pdErrNeedPassword)
+            {
+                std::wcout << L"...But still requires a password [FAILURE]." << std::endl;
+            }
+            else
+            {
+                std::wcout << L"An unexpected error occured." << std::endl;
+                ASRaise(openError);
+            }
+        }
+        else
+        {
+            std::wcout << L"...And is no longer encrypted!" << std::endl;
             PDDocClose(unencrypted_document);
-		}
+        }
 
-	HANDLER
-		errCode = ERRORCODE;
-	END_HANDLER
+    HANDLER
+    
+    errCode = ERRORCODE;
 
-	//Release created objects
-    if (encrypted_document) PDDocClose(encrypted_document);
+    END_HANDLER
+
+
+    //Release created objects
+    if (encrypted_document)   PDDocClose(encrypted_document);
     if (unencrypted_document) PDDocClose(unencrypted_document);
     ASFileSysReleasePath(ASGetDefaultFileSys(), encrypted_path);
 
-	//If there was an error, display it
-	if (errCode) DisplayError(errCode);
-	
-	//Terminate the APDFL library
-	MyPDFLTerm();
+    //If there was an error, display it
+    if (errCode) DisplayError(errCode);
 
-	//End
-	return errCode;
-}
+    //Terminate the APDFL library
+    MyPDFLTerm();
+
+    //End
+    return errCode;
+};
+
+//==================================================================
+//Called by PDDocOpenEx to obtain permission to open the document
+//by supplying the password.
+//==================================================================
+static ACCB1 ASBool ACCB2 openAuthorizationProcedure(PDDoc encrypted, void *clientData){
+
+    PDPermReqStatus permReqStatus; //Stores the result of the permission request
+
+    DURING
+
+        //Request open permission by supplying the password
+        permReqStatus = PDDocPermRequest(encrypted,
+                            PDPermReqObjDoc,  //Object of the request: a document
+                            PDPermReqOprOpen, //Target operation of the request: to open
+                            (void*)password);
+    HANDLER
+
+        RERAISE();
+
+    END_HANDLER
+
+    switch (permReqStatus)
+    {
+        case PDPermReqGranted:
+            std::wcout << L"openAuthorizationProcedure: Password verified.\nPermission to open the document has been granted." << std::endl;
+            break;
+        case PDPermReqDenied:
+            std::wcout << L"openAuthorizationProcedure: Invalid password.\nThe request to open the document has been denied." << std::endl;
+            break;
+        case PDPermReqUnknownObject:
+            std::wcout << L"openAuthorizationProcedure: Target object unknown for the permisson request." << std::endl;
+            break;
+        case PDPermReqUnknownOperation:
+            std::wcout << L"openAuthorizationProcedure: Target operation unknown for the permission request." << std::endl;
+            break;
+        default:
+            std::wcout << L"openAuthorizationProcedure: An unexpected error occured while trying to open the document." << std::endl;
+            break;
+    }
+
+    return (permReqStatus == PDPermReqGranted);
+};
