592b3db26842e9b940261bc3578ad56ba21bd214
Jordan Marks <jmarks@datalogics.com>
2015-07-13 13:09:20 -0500

    Rename init -> Init

---
+++ Init/InitializeLibrary.h
@@ -0,0 +1,84 @@
+// Copyright(c) 2015, Datalogics, Inc.All rights reserved.
+
+//========================================================================
+// Sample: Initialize - Initializes the APDFLibrary.
+//========================================================================
+
+// This agreement is between Datalogics, Inc. 101 N.Wacker Drive, Suite 1800,
+// Chicago, IL 60606 ("Datalogics") and you, an end user who downloads
+// source code examples for integrating to the Adobe PDF Library
+// ("the Example Code"). By accepting this agreement you agree to be bound
+// by the following terms of use for the Example Code.
+//
+// LICENSE
+// -------
+// Datalogics hereby grants you a royalty - free, non - exclusive license to
+// download and use the Example Code for any lawful purpose.There is no charge
+// for use of Example Code.
+//
+// OWNERSHIP
+// ---------
+// The Example Code and any related documentation and trademarks are and shall
+// remain the sole and exclusive property of Datalogics and are protected by
+// the laws of copyright in the U.S.and other countries.
+//
+// Datalogics is a trademark of Datalogics, Inc.
+//
+// TERM
+// ----
+// This license is effective until terminated.You may terminate it at any
+// other time by destroying the Example Code.
+//
+// WARRANTY DISCLAIMER
+// -------------------
+// THE EXAMPLE CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// DATALOGICS DISCLAIM ALL OTHER WARRANTIES, CONDITIONS, UNDERTAKINGS OR
+// TERMS OF ANY KIND, EXPRESS OR IMPLIED, WRITTEN OR ORAL, BY OPERATION OF
+// LAW, ARISING BY STATUTE, COURSE OF DEALING, USAGE OF TRADE OR OTHERWISE,
+// INCLUDING, WARRANTIES OR CONDITIONS OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE, SATISFACTORY QUALITY, LACK OF VIRUSES, TITLE,
+// NON - INFRINGEMENT, ACCURACY OR COMPLETENESS OF RESPONSES, RESULTS, AND / OR
+// LACK OF WORKMANLIKE EFFORT.THE PROVISIONS OF THIS SECTION SET FORTH
+// SUBLICENSEE'S SOLE REMEDY AND DATALOGICS'S SOLE LIABILITY WITH RESPECT
+// TO THE WARRANTY SET FORTH HEREIN.NO REPRESENTATION OR OTHER AFFIRMATION
+// OF FACT, INCLUDING STATEMENTS REGARDING PERFORMANCE OF THE EXAMPLE CODE,
+// WHICH IS NOT CONTAINED IN THIS AGREEMENT, SHALL BE BINDING ON DATALOGICS.
+// NEITHER DATALOGICS WARRANT AGAINST ANY BUG, ERROR, OMISSION, DEFECT,
+// DEFICIENCY, OR NONCONFORMITY IN ANY EXAMPLE CODE.
+
+#ifndef INITLIB_H
+#define INITLIB_H
+
+#define NUM_FONTS 2
+#define NUM_COLOR_PROFS 1
+#define NUM_PLUGIN_DIRS 1
+
+#include "PDFLCalls.h"
+#include <iostream>
+
+class APDFLib
+{
+public:
+    APDFLib();
+    ~APDFLib();
+
+    ASInt32 getInitError();
+    ASBool isValid();
+    static void displayError(ASErrorCode errCode);
+
+private:
+    PDFLDataRec pdflData;
+    ASInt32 initError;
+    ASBool initValid;
+
+    void fillDirectories();
+
+    ASUTF16Val* fontDirList[NUM_FONTS];                //TODO: platform divergences
+    ASUTF16Val* colorProfDirList[NUM_COLOR_PROFS];     //TODO: platform divergences
+    ASUTF16Val* pluginDirList[NUM_PLUGIN_DIRS];        //TODO: platform divergences
+};
+
+#endif //INITLIB_H
