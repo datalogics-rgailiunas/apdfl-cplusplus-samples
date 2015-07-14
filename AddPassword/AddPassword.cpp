7ab9073f4944d267640560068702ce7122120ab5
Jordan Marks <jmarks@datalogics.com>
2015-07-14 10:56:03 -0500

    AddPassword sample. - Uses our initializer - Uses a copy of SetUniquePermissions.pdf as input

---
+++ AddPassword/AddPassword.cpp
@@ -0,0 +1,142 @@
+// Copyright(c) 2015, Datalogics, Inc.All rights reserved.
+
+//==========================================================================
+// Sample: AddPassword - Adds a password to open an otherwise unsecured
+//          document.
+//
+//Steps: 
+//1) Open the document and create new security data, selecting the password
+//2) Set the security data into the document
+//3) Save and close the document
+//==========================================================================
+
+// This agreement is between Datalogics, Inc. 101 N.Wacker Drive, Suite 1800,
+// Chicago, IL 60606 ("Datalogics") and you, an end user who downloads
+// source code examples for integrating to the Adobe PDF Library
+// ("the Example Code"). By accepting this agreement you agree to be bound
+// by the following terms of use for the Example Code.
+//
+// LICENSE
+// -------
+// Datalogics hereby grants you a royalty - free, non - exclusive license to
+// download and use the Example Code for any lawful purpose.There is no charge
+// for use of Example Code.
+//
+// OWNERSHIP
+// ---------
+// The Example Code and any related documentation and trademarks are and shall
+// remain the sole and exclusive property of Datalogics and are protected by
+// the laws of copyright in the U.S.and other countries.
+//
+// Datalogics is a trademark of Datalogics, Inc.
+//
+// TERM
+// ----
+// This license is effective until terminated.You may terminate it at any
+// other time by destroying the Example Code.
+//
+// WARRANTY DISCLAIMER
+// -------------------
+// THE EXAMPLE CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// DATALOGICS DISCLAIM ALL OTHER WARRANTIES, CONDITIONS, UNDERTAKINGS OR
+// TERMS OF ANY KIND, EXPRESS OR IMPLIED, WRITTEN OR ORAL, BY OPERATION OF
+// LAW, ARISING BY STATUTE, COURSE OF DEALING, USAGE OF TRADE OR OTHERWISE,
+// INCLUDING, WARRANTIES OR CONDITIONS OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE, SATISFACTORY QUALITY, LACK OF VIRUSES, TITLE,
+// NON - INFRINGEMENT, ACCURACY OR COMPLETENESS OF RESPONSES, RESULTS, AND / OR
+// LACK OF WORKMANLIKE EFFORT.THE PROVISIONS OF THIS SECTION SET FORTH
+// SUBLICENSEE'S SOLE REMEDY AND DATALOGICS'S SOLE LIABILITY WITH RESPECT
+// TO THE WARRANTY SET FORTH HEREIN.NO REPRESENTATION OR OTHER AFFIRMATION
+// OF FACT, INCLUDING STATEMENTS REGARDING PERFORMANCE OF THE EXAMPLE CODE,
+// WHICH IS NOT CONTAINED IN THIS AGREEMENT, SHALL BE BINDING ON DATALOGICS.
+// NEITHER DATALOGICS WARRANT AGAINST ANY BUG, ERROR, OMISSION, DEFECT,
+// DEFICIENCY, OR NONCONFORMITY IN ANY EXAMPLE CODE.
+
+#include <iostream>
+#include "SampleUtils.h"
+#include "../Common/Init/InitializeLibrary.h"
+
+int main(int argc, char** argv)
+{
+    APDFLib lib;                   //Constructor initializes the APDF Library.
+    int err = lib.getInitError(); //Will display the error, if any.
+    if (err) return err;
+ 
+    Utilities util;                                          //Performs common functions.
+    ASErrorCode errCode = 0;                                 //Tracks runtime errors in the application
+    const wchar_t* inPath  = L"../Input/AddPassword.pdf";    //Input document path
+    const wchar_t* outPath = L"AddPassword_Out.pdf";         //Output document path
+    const char*   password = "Datalogics";                   //Password to open document (cannot be wide char)
+
+    DURING
+
+//=========================================================================================================================
+//Step 1) Open the document and create new security data, selecting the password
+//=========================================================================================================================
+
+        std::cout << "Opening the input document." << std::endl;
+
+        PDDoc document = util.openPDFNoSecurity(inPath);
+
+        std::cout << "Creating the new security data." << std::endl;
+
+        //This structure will hold the new security data
+        PDDocSetNewCryptHandler(document, ASAtomFromString("Standard"));   //Prepare to create new security data
+        StdSecurityData securityData = (StdSecurityData)PDDocNewSecurityData(document);    //...and create it
+        securityData->size = sizeof(StdSecurityDataRec);
+
+        //The password needed to open the output file
+        securityData->hasUserPW = true;
+        securityData->newUserPW = true;
+        strcpy(securityData->userPW, password);
+
+        //No password necessary to change restrictions (there will be no restrictions once the file is opened)
+        securityData->hasOwnerPW = false;
+
+        //Set the encryption method
+        //2 = CF_METHOD_RC4_V2 - RC4 algorithm
+        //5 = CF_METHOD_AES_V1 - AES algorithm with a zero initialization vector
+        //6 = CF_METHOD_AES_V2 - AES algorithm with a 16 byte random initialization vector
+        //7 = CF_METHOD_AES_V3 - AES algorithm with a 4 byte random initialization vector
+        securityData->encryptMethod = 2;
+        securityData->keyLength = 16;    //encryption key length in bytes
+        securityData->encryptMetadata = true;
+        securityData->encryptAttachmentsOnly = false;
+
+        //The user will be able to perform all operations when the file is opened.
+        securityData->perms = pdPermOwner;
+
+        std::cout << "New security permissions have been set...." << std::endl;
+
+//=========================================================================================================================
+//Step 2) Set the security data into the document
+//=========================================================================================================================
+
+        PDDocSetNewSecurityData(document, (void*)securityData);
+        PDDocSetFlags(document, PDDocRequiresFullSave );           //Changing the security data requires a full save
+        ASfree((void*)securityData);
+
+        std::cout << "...and added to the document." << std::endl;
+
+//=========================================================================================================================
+//Step 3) Save and close the document
+//=========================================================================================================================
+
+        std::cout << "Saving the new file." << std::endl;
+        PDDocSave(document, PDSaveFull | PDSaveLinearized, util.makeASPathName(outPath), ASGetDefaultFileSys(), NULL, NULL);
+        PDDocClose(document);
+
+    HANDLER
+
+        errCode = ERRORCODE;
+
+    END_HANDLER
+
+    if (errCode)
+        lib.displayError(errCode);
+
+    return errCode;    //End. lib's destructor terminates the library.
+};
---
+++ AddPassword/SampleUtils.cpp
@@ -0,0 +1,151 @@
+// Copyright (c) 2015, Datalogics, Inc. All rights reserved.
+
+//************************************************************************
+// Sample: Bookmarks - Performs common functions for the sample
+//
+// This class performs several repeatedly used functions which are
+// not critical for the sample to perform explicitly.
+//************************************************************************
+
+// This agreement is between Datalogics, Inc. 101 N. Wacker Drive, Suite 1800,
+// Chicago, IL 60606 ("Datalogics") and you, an end user who downloads
+// source code examples for integrating to the Adobe PDF Library
+// ("the Example Code"). By accepting this agreement you agree to be bound
+// by the following terms of use for the Example Code.
+//
+// LICENSE
+// -------
+// Datalogics hereby grants you a royalty-free, non-exclusive license to
+// download and use the Example Code for any lawful purpose. There is no charge
+// for use of Example Code.
+//
+// OWNERSHIP
+// ---------
+// The Example Code and any related documentation and trademarks are and shall
+// remain the sole and exclusive property of Datalogics and are protected by
+// the laws of copyright in the U.S. and other countries.
+//
+// Datalogics is a trademark of Datalogics, Inc.
+//
+// TERM
+// ----
+// This license is effective until terminated. You may terminate it at any
+// other time by destroying the Example Code.
+//
+// WARRANTY DISCLAIMER
+// -------------------
+// THE EXAMPLE CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// DATALOGICS DISCLAIM ALL OTHER WARRANTIES, CONDITIONS, UNDERTAKINGS OR
+// TERMS OF ANY KIND, EXPRESS OR IMPLIED, WRITTEN OR ORAL, BY OPERATION OF
+// LAW, ARISING BY STATUTE, COURSE OF DEALING, USAGE OF TRADE OR OTHERWISE,
+// INCLUDING, WARRANTIES OR CONDITIONS OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE, SATISFACTORY QUALITY, LACK OF VIRUSES, TITLE,
+// NON-INFRINGEMENT, ACCURACY OR COMPLETENESS OF RESPONSES, RESULTS, AND/OR
+// LACK OF WORKMANLIKE EFFORT. THE PROVISIONS OF THIS SECTION SET FORTH
+// SUBLICENSEE'S SOLE REMEDY AND DATALOGICS'S SOLE LIABILITY WITH RESPECT
+// TO THE WARRANTY SET FORTH HEREIN. NO REPRESENTATION OR OTHER AFFIRMATION
+// OF FACT, INCLUDING STATEMENTS REGARDING PERFORMANCE OF THE EXAMPLE CODE,
+// WHICH IS NOT CONTAINED IN THIS AGREEMENT, SHALL BE BINDING ON DATALOGICS.
+// NEITHER DATALOGICS WARRANT AGAINST ANY BUG, ERROR, OMISSION, DEFECT,
+// DEFICIENCY, OR NONCONFORMITY IN ANY EXAMPLE CODE.
+
+#include "SampleUtils.h"
+
+//==================================================================
+//Constructor computes the host's unicode format.
+//==================================================================
+Utilities::Utilities(){
+    if (sizeof(wchar_t) == 2)
+        hostUniFormat = kUTF16HostEndian;
+    else
+        hostUniFormat = kUTF32HostEndian;
+};
+
+//==================================================================
+//Convert a string to a wide string. The caller must delete[]
+//the returned array.
+//==================================================================
+wchar_t* Utilities::toWide(const char* str){
+
+    const size_t strlen = (std::strlen(str)) + 1;
+    wchar_t* wstr = new wchar_t[strlen];
+    mbstowcs(wstr, str, strlen);
+    return wstr;
+};
+
+//==================================================================
+//Convert a wide string to an ASText object.
+//==================================================================
+ASText Utilities::toASText(const wchar_t* string){
+    return ASTextFromUnicode((ASUTF16Val *)string, hostUniFormat);
+};
+//==================================================================
+//Create an ASPathName from a const wchar_t*.
+//==================================================================
+ASPathName Utilities::makeASPathName(const wchar_t* pathname){
+
+    ASText pathText = NULL;         //Text of pathname
+    ASPathName pathASPath = NULL;   //ASPathName for path
+
+    DURING
+        pathText = ASTextFromUnicode((ASUTF16Val *)pathname, hostUniFormat);
+    pathASPath = ASFileSysCreatePathFromDIPathText(NULL, pathText, NULL);
+    HANDLER
+        RERAISE();
+    END_HANDLER
+
+        //Release resources
+        ASTextDestroy(pathText);
+
+    return pathASPath;
+};
+
+//==================================================================
+//Open a file.
+//==================================================================
+ASFile Utilities::openASFile(wchar_t* filepath){
+    ASErrorCode errCode = 0;       //Tracks errors
+    ASFile file = NULL;            //The file we want to open
+    ASPathName aspfilepath = makeASPathName(filepath);    //Filepath of the document
+
+    DURING
+        errCode = ASFileSysOpenFile(
+        ASGetDefaultFileSys(), //ASFileSys
+        aspfilepath,           //ASPathName
+        ASFILE_READ,           //ASFileMode
+        &file);                //ASFile*, filled by ASFileSysOpenFile
+    HANDLER
+        RERAISE();
+    END_HANDLER
+
+    if (errCode)
+        ASRaise(errCode);
+
+    //Release resources
+    ASFileSysReleasePath(ASGetDefaultFileSys(), aspfilepath);
+
+    return file;
+};
+
+//==================================================================
+//Open an unprotected PDF file
+//==================================================================
+PDDoc Utilities::openPDFNoSecurity(const wchar_t* filepath){
+
+    ASPathName doc_path = makeASPathName(filepath);    //Filepath of the document
+    PDDoc doc = NULL;                                  //Reference to the document
+
+    DURING
+        doc = PDDocOpen(doc_path, NULL, NULL, true);
+    HANDLER
+        ASRaise(ERRORCODE);
+    END_HANDLER
+
+    //Release resources
+    ASFileSysReleasePath(ASGetDefaultFileSys(), doc_path);
+
+    return doc;
+};
