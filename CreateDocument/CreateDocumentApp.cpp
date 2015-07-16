67c8633f42ca07221a24376c8fb091e449bb98c0
Jeremy Savarese <jsavarese@datalogics.com>
2015-07-16 15:12:41 -0500

    Changes to comments and HANDLER

--- CreateDocument/CreateDocumentApp.cpp
+++ CreateDocument/CreateDocumentApp.cpp
@@ -1,6 +1,6 @@
-//# Copyright(c) 2015, Datalogics, Inc.All rights reserved.
+// Copyright(c) 2015, Datalogics, Inc.All rights reserved.
 
-//************************************************************************
+//========================================================================
 // Sample: Create Document - Creates a document with page numbers
 //
 // Note: This program creates a new document and places page numbers
@@ -12,52 +12,52 @@
 //Steps:
 // 1) Create Document and add pages
 // 2) Save the document and release resources
-//************************************************************************
+//========================================================================
 
-//# This agreement is between Datalogics, Inc. 101 N.Wacker Drive, Suite 1800,
-//# Chicago, IL 60606 ("Datalogics") and you, an end user who downloads
-//# source code examples for integrating to the Adobe PDF Library
-//# ("the Example Code"). By accepting this agreement you agree to be bound
-//# by the following terms of use for the Example Code.
-//#
-//# LICENSE
-//# -------
-//# Datalogics hereby grants you a royalty - free, non - exclusive license to
-//# download and use the Example Code for any lawful purpose.There is no charge
-//# for use of Example Code.
-//#
-//# OWNERSHIP
-//# ---------
-//# The Example Code and any related documentation and trademarks are and shall
-//# remain the sole and exclusive property of Datalogics and are protected by
-//# the laws of copyright in the U.S.and other countries.
-//#
-//# Datalogics is a trademark of Datalogics, Inc.
-//#
-//# TERM
-//# ----
-//# This license is effective until terminated.You may terminate it at any
-//# other time by destroying the Example Code.
-//#
-//# WARRANTY DISCLAIMER
-//# -------------------
-//# THE EXAMPLE CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-//# EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES
-//# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-//#
-//# DATALOGICS DISCLAIM ALL OTHER WARRANTIES, CONDITIONS, UNDERTAKINGS OR
-//# TERMS OF ANY KIND, EXPRESS OR IMPLIED, WRITTEN OR ORAL, BY OPERATION OF
-//# LAW, ARISING BY STATUTE, COURSE OF DEALING, USAGE OF TRADE OR OTHERWISE,
-//# INCLUDING, WARRANTIES OR CONDITIONS OF MERCHANTABILITY, FITNESS FOR A
-//# PARTICULAR PURPOSE, SATISFACTORY QUALITY, LACK OF VIRUSES, TITLE,
-//# NON - INFRINGEMENT, ACCURACY OR COMPLETENESS OF RESPONSES, RESULTS, AND / OR
-//# LACK OF WORKMANLIKE EFFORT.THE PROVISIONS OF THIS SECTION SET FORTH
-//# SUBLICENSEE'S SOLE REMEDY AND DATALOGICS'S SOLE LIABILITY WITH RESPECT
-//# TO THE WARRANTY SET FORTH HEREIN.NO REPRESENTATION OR OTHER AFFIRMATION
-//# OF FACT, INCLUDING STATEMENTS REGARDING PERFORMANCE OF THE EXAMPLE CODE,
-//# WHICH IS NOT CONTAINED IN THIS AGREEMENT, SHALL BE BINDING ON DATALOGICS.
-//# NEITHER DATALOGICS WARRANT AGAINST ANY BUG, ERROR, OMISSION, DEFECT,
-//# DEFICIENCY, OR NONCONFORMITY IN ANY EXAMPLE CODE. */
+// This agreement is between Datalogics, Inc. 101 N.Wacker Drive, Suite 1800,
+// Chicago, IL 60606 ("Datalogics") and you, an end user who downloads
+// source code examples for integrating to the Adobe PDF Library
+// ("the Example Code"). By accepting this agreement you agree to be bound
+// by the following terms of use for the Example Code.
+//
+// LICENSE
+// -------
+// Datalogics hereby grants you a royalty - free, non - exclusive license to
+// download and use the Example Code for any lawful purpose.There is no charge
+// for use of Example Code.
+//
+// OWNERSHIP
+// ---------
+// The Example Code and any related documentation and trademarks are and shall
+// remain the sole and exclusive property of Datalogics and are protected by
+// the laws of copyright in the U.S.and other countries.
+//
+// Datalogics is a trademark of Datalogics, Inc.
+//
+// TERM
+// ----
+// This license is effective until terminated.You may terminate it at any
+// other time by destroying the Example Code.
+//
+// WARRANTY DISCLAIMER
+// -------------------
+// THE EXAMPLE CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// DATALOGICS DISCLAIM ALL OTHER WARRANTIES, CONDITIONS, UNDERTAKINGS OR
+// TERMS OF ANY KIND, EXPRESS OR IMPLIED, WRITTEN OR ORAL, BY OPERATION OF
+// LAW, ARISING BY STATUTE, COURSE OF DEALING, USAGE OF TRADE OR OTHERWISE,
+// INCLUDING, WARRANTIES OR CONDITIONS OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE, SATISFACTORY QUALITY, LACK OF VIRUSES, TITLE,
+// NON - INFRINGEMENT, ACCURACY OR COMPLETENESS OF RESPONSES, RESULTS, AND / OR
+// LACK OF WORKMANLIKE EFFORT.THE PROVISIONS OF THIS SECTION SET FORTH
+// SUBLICENSEE'S SOLE REMEDY AND DATALOGICS'S SOLE LIABILITY WITH RESPECT
+// TO THE WARRANTY SET FORTH HEREIN.NO REPRESENTATION OR OTHER AFFIRMATION
+// OF FACT, INCLUDING STATEMENTS REGARDING PERFORMANCE OF THE EXAMPLE CODE,
+// WHICH IS NOT CONTAINED IN THIS AGREEMENT, SHALL BE BINDING ON DATALOGICS.
+// NEITHER DATALOGICS WARRANT AGAINST ANY BUG, ERROR, OMISSION, DEFECT,
+// DEFICIENCY, OR NONCONFORMITY IN ANY EXAMPLE CODE. */
 
 #include <iostream>
 
@@ -72,7 +72,7 @@ int main(int argc, char** argv)
     ASErrorCode errCode = 0;                     //Error code is 0 if there were no issues 
 
     if (libInit.isValid() == false)              //If there was a problem in initializing the APDFL
-        return errCode = libInit.getInitError(); //Return the error code.
+        return libInit.getInitError(); //Return the error code.
         
     DURING
 
@@ -125,7 +125,9 @@ int main(int argc, char** argv)
 
     HANDLER
 
-        libInit.displayError(errCode); //Display any exceptions that may have occured.
+        errCode = libInit.getInitError(); //Get the error code
+
+        libInit.displayError(errCode);    //Display any exceptions that may have occured.
 
     END_HANDLER
 
