b87430700b5a0e0bc907fcd7b07fc6834a30226e
Jordan Marks <jmarks@datalogics.com>
2015-07-22 09:34:30 -0500

    Now Englishes better.

--- LockDocument/LockDocument.cpp
+++ LockDocument/LockDocument.cpp
@@ -60,7 +60,7 @@
 
 int main(int argc, char** argv)
 {
-    //Initialize the APDF Library.
+    //Initialize the Adobe PDF Library.
     APDFLib lib;
     if (!lib.isValid())
         return lib.getInitError();    //Will display the error, if any.
