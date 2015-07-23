3faf4b181f5fe3acb8c60e369d66c3c25c133d14
Jordan Marks <jmarks@datalogics.com>
2015-07-23 14:41:06 -0500

    Stylized. Mostly comment add/fix..

--- _Common/Init/InitializeLibrary.h
+++ _Common/Init/InitializeLibrary.h
@@ -1,7 +1,12 @@
 // Copyright(c) 2015, Datalogics, Inc.All rights reserved.
 
 //========================================================================
-// Sample: Initialize - Initializes the APDFLibrary.
+// Sample - Initialize: This class defines an object used for the
+// initialization and termination of the Adobe PDF Library. It will also
+// report initialization errors.
+//
+// InitializeLibrary.cpp: Contains the method implementations.
+// InitializeLibrary.h: Contains the class definition.
 //========================================================================
 
 // This agreement is between Datalogics, Inc. 101 N.Wacker Drive, Suite 1800,
@@ -52,33 +57,33 @@
 #ifndef INITLIB_H
 #define INITLIB_H
 
-#define NUM_FONTS 2
-#define NUM_COLOR_PROFS 1
-#define NUM_PLUGIN_DIRS 1
+#define NUM_FONTS 2          //The number of font directories we'll include during initialization.    
+#define NUM_COLOR_PROFS 1    //The number of color profile directories we'll include during initialization.
+#define NUM_PLUGIN_DIRS 1    //The number of plugin directories we'll include during initialization.
 
-#include "PDFLCalls.h"
 #include <iostream>
+#include "PDFLCalls.h"
 
 class APDFLib
 {
 public:
-    APDFLib();
-    ~APDFLib();
+    APDFLib();                                        //Constructor initializes APDFL.
+    ~APDFLib();                                       //Destructor terminates APDFL.
 
-    ASInt32 getInitError();                        //Reports error during initialization and returns error code.
-    ASBool isValid() { return initValid; };        //Returns true if library did initialize.
-    void displayError(ASErrorCode);
+    ASInt32 getInitError();                           //Reports whether an error happened during initialization and returns that error.
+    ASBool isValid() { return initValid; };           //Returns true if the library initialized successfully.
+    void displayError(ASErrorCode);                   //Utility method, may be used to print APDFL errors to the terminal.
 
 private:
-    PDFLDataRec pdflData;                          //Struct containing information to initialize APDFL
-    ASInt32 initError;                             //Used to report errors in initialization
-    ASBool initValid;                              //true if library did initialize
+    PDFLDataRec pdflData;                             //A struct containing information that APDFL initializes with.
+    ASInt32 initError;                                //Used to record initialization errors.
+    ASBool initValid;                                 //Set to true if the library initializes successfully.
 
-    void fillDirectories();
+    void fillDirectories();                           //Sets directory information for our PDFLDataRec.
 
-    ASUTF16Val* fontDirList[NUM_FONTS];                //TODO: platform divergences
-    ASUTF16Val* colorProfDirList[NUM_COLOR_PROFS];     //TODO: platform divergences
-    ASUTF16Val* pluginDirList[NUM_PLUGIN_DIRS];        //TODO: platform divergences
+    ASUTF16Val* fontDirList[NUM_FONTS];               //List of font directories we'll include during initialization.              //TODO: platform divergences
+    ASUTF16Val* colorProfDirList[NUM_COLOR_PROFS];    //List of color profile directories we'll include during initialization.     //TODO: platform divergences
+    ASUTF16Val* pluginDirList[NUM_PLUGIN_DIRS];       //List of plugin directories we'll include during initialization.            //TODO: platform divergences
 };
 
 #endif //INITLIB_H
