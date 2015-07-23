f0285cdc8ed9e2a5b7c14d45435c80a8bbc9c913
Jeremy Savarese <jsavarese@datalogics.com>
2015-07-23 15:08:41 -0500

    AddArt: Changes made to comments.

--- AddArt/AddArt.cpp
+++ AddArt/AddArt.cpp
@@ -1,18 +1,15 @@
 // Copyright(c) 2015, Datalogics, Inc.All rights reserved.
 
 //==============================================================================
-// Sample: AddArt, this sample demonstrates creating custom objects in
-// a PDDocument
-// Note: This program creates an output document in the working directory
-// with some basic shapes inserted into it.
+// Sample: AddArt - This sample demonstrates drawing an object by manipulating
+// PDEPath objects.
 //	
-//Steps:
-// 1) Create a new PDF Document, Insert Page, Acquire PDEContent
-// 2) Set the PDEGraphicsState object containing information about how the 
-//    shape will be displayed.
-// 3) Create the shape from a list of coordinates.
-// 4) Create a copy of the shape and fill in/transform it.
-// 5) Save the PDF Document, catch any exceptions and terminate the APDFL
+// Steps:
+// 1) Create a PDF document with one page.
+// 2) Set the graphics state for the first shape.
+// 3) Draw the arrow.
+// 4) Draw a second arrow and apply transformations.
+// 5) Release resources and save the PDF document.
 //==============================================================================
 
 // This agreement is between Datalogics, Inc. 101 N.Wacker Drive, Suite 1800,
@@ -72,72 +69,74 @@
 int main(int argc, char** argv)
 {
 
-    //Initialize the APDFL and check for errors
-    APDFLib libInit;
-    ASErrorCode errCode = 0;
+    APDFLib libInit;                   //Initialize the Adobe PDF Library.
+    ASErrorCode errCode = 0;           //Variable used to report any exceptions/errors if they occured.
 
-    if (libInit.isValid() == false)
+    if (libInit.isValid() == false)    //If there was a problem in initialization, return the error code.
         return libInit.getInitError();
 
-//================================================================================================================================
-//Step 1) Create a new PDF Document, Insert Page, Acquire Content
-//================================================================================================================================
+//=============================================================================================================================================================
+//Step 1: Create a PDF document with one page. After the document is created a page and a list of the page's content is acquired.
+//=============================================================================================================================================================
+
     DURING
 
-        wchar_t* pathName = L"AddArtOut.pdf"; //Output file path
+        wchar_t* pathName = L"AddArtOut.pdf";                          //This variable holds the output file's pathname.
 
-        APDFLDoc document;  //Create a new document
+        APDFLDoc document;                                             //Call APDFLDoc's default constructor to create a new PDF document.
 
-        document.insertPage(Int32ToFixed((72 * 10)), Int32ToFixed((72 * 10)), PDBeforeFirstPage);   //Insert a 10 inch by 10 inch page
+        //Insert a 10 x 10 inch page into the PDF document.
+        document.insertPage(Int32ToFixed((72 * 10)), Int32ToFixed((72 * 10)), PDBeforeFirstPage);    
     
-        PDPage pdPage = document.getPage(0);                  //Retrieve the page that was just created.
+        PDPage pdPage = document.getPage(0);                           //Retrieve the page that was just created.
 
-        PDEContent pdeContent = PDPageAcquirePDEContent(pdPage, 0); //Acquire list of contents from page
+        PDEContent pdeContent = PDPageAcquirePDEContent(pdPage, 0);    //Acquire the content list from the PDPage.
 
-        PDEPath pdePath = PDEPathCreate();                          //Create path object, which be made into a shape
+        PDEPath pdePath = PDEPathCreate();                             //Create path object, which will be made into a shape.
 
-//================================================================================================================================
-//Step 2) Set the PDEGraphicsState attributes of the PDEPath, this structure will determine display information for the PDEPath 
-//object such as color, line width line joins, etc.
-//================================================================================================================================
-        
-        const short numberOfPoints = 7;                         //Number of points required to draw an arrow
+//=============================================================================================================================================================
+// Step 2: Set the graphics state for the first shape. This PDEGraphicsState object contains attributes about how a PDEElement will be displayed.
+//============================================================================================================================================================= 
 
-        PDEPathSetPaintOp(pdePath, kPDEStroke);                 //PDEPath will be stroked, but not filled
+        PDEPathSetPaintOp(pdePath, kPDEStroke);                 //PDEPath will be stroked, but not filled.
 
-        PDEGraphicState gState;                                 //Structure determines display information for the PDEPath
-        memset(&gState, 0, sizeof(gState));                     //Initially values will be 0
+        PDEGraphicState gState;                                 //Struct that will hold display attributes.
+        memset(&gState, 0, sizeof(gState));                     //Clear out any "garbage data" in the struct.
 
-        ASFixed red = ASFloatToFixed(.6);                       //Red Intensity                              
-        ASFixed green = ASFloatToFixed(.2);                     //Green Intensity
-        ASFixed blue = ASFloatToFixed(1.0);                     //Blue Intensity
+        ASFixed red = ASFloatToFixed(.6);                       //Red Intensity ranges is [0.0 - 1.0].                             
+        ASFixed green = ASFloatToFixed(.2);                     //Green Intensity ranges is [0.0 - 1.0].
+        ASFixed blue = ASFloatToFixed(1.0);                     //Blue Intensity ranges is [0.0 - 1.0].
 
-        PDEColorValue strokeColorValue;                         //Will be assigned to PDEGraphicState.strokeColorSpec
+        PDEColorValue strokeColorValue;                         //Will be assigned to PDEGraphicState.strokeColorSpec.
 
-        memset(&strokeColorValue, 0, sizeof(PDEColorValue));    //Initially value = 0 (Black)
-        strokeColorValue.color[0] = red;                        //Assign values to the struct
-        strokeColorValue.color[1] = green;                      //Values provided will show purple
+        memset(&strokeColorValue, 0, sizeof(PDEColorValue));    //Initially value = 0 (Black.)
+        strokeColorValue.color[0] = red;                        //Assign the color values we just set to the struct.
+        strokeColorValue.color[1] = green;                      //In this case the object will be painted purple.
         strokeColorValue.color[2] = blue;
 
-        PDEColorSpace colorSpace = PDEColorSpaceCreateFromName(ASAtomFromString("DeviceRGB")); //RGB color space
+        //We are using the RGB color space in this case. Default value is "DeviceGray".
+        PDEColorSpace colorSpace = PDEColorSpaceCreateFromName(ASAtomFromString("DeviceRGB"));   
 
-        PDEColorSpec strokeColorSpec = {colorSpace, strokeColorValue}; //Struct initializes gState.strokeColorSpec
+        //Set the PDEColorSpec struct with the colorSpace and strokeColorValues we just set.
+        PDEColorSpec strokeColorSpec = {colorSpace, strokeColorValue};    
 
-        gState.strokeColorSpec = strokeColorSpec; //Set to "DeviceRBG" and Purple
-        gState.lineWidth = Int32ToFixed(3);       //Line width is 1
-        gState.lineCap = 1;                       //Rounded line caps
-        gState.lineJoin = 1;                      //Rounded line joins
-        gState.miterLimit = fixedTen;             //Set to default value
-        gState.flatness = fixedZero;              //Set to default value
+        gState.strokeColorSpec = strokeColorSpec;               //Set to "DeviceRBG" and purple.
+        gState.lineWidth = Int32ToFixed(3);                     //Line width is set to a thickness of 3.
+        gState.lineCap = 1;                                     //Using rounded line caps.
+        gState.lineJoin = 1;                                    //Using rounded line joins.
+        gState.miterLimit = fixedTen;                           //Set this to it's default value.
+        gState.flatness = fixedZero;                            //Set this to it's default value.
 
         //Set the PDEPath's graphic state with the structure that was just filled out.
         PDEElementSetGState(reinterpret_cast<PDEElement>(pdePath), &gState, sizeof(PDEGraphicState));
 
-//================================================================================================================================
-//Step 3) Create an arrow from a list of coordinates by calling PDEPathAddSegment to join the X,Y coordinates with straight lines.
-//================================================================================================================================
+//=============================================================================================================================================================
+//Step 3: Draw the arrow. This shape is drawn using a list of coordinates and calling PDEPathAddSegment to join the (x, y) coordinates with straight lines.
+//=============================================================================================================================================================
 
-        //(X, Y) coordinates for points on the arrow that is being generated. (72 pixels in one inch)
+        const short numberOfPoints = 7;    //Number of points required to draw an arrow
+
+        //Arrays containing (x, y) coordinates representing points for the arrow that is being drawn. (72 pixels in one inch.)
         ASInt32 arrowXCoordinates[numberOfPoints] {(72 * 7), (72 * 6), (72 * 6), (72 * 4), (72 * 4), (72 * 3), (72 * 5)};
         ASInt32 arrowYCoordinates[numberOfPoints] {(72 * 7), (72 * 7), (72 * 3), (72 * 3), (72 * 7), (72 * 7), (72 * 9)};
         ASFixedPoint pointToInsert; 
@@ -147,7 +146,7 @@ int main(int argc, char** argv)
         pointToInsert.v = ASInt32ToFixed(72 * 9);
         PDEPathAddSegment(pdePath, kPDEMoveTo, pointToInsert.h, pointToInsert.v, fixedZero, fixedZero, fixedZero, fixedZero);
 
-        //Iterate through the coordinates and draw lines connecting each (X,Y) pair.
+        //Loop through the coordinates and draw lines connecting each (x, y) pair.
         for (unsigned i = 0; i < numberOfPoints; ++i)
         {
             pointToInsert.h = ASInt32ToFixed(arrowXCoordinates[i]);
@@ -156,42 +155,43 @@ int main(int argc, char** argv)
             PDEPathAddSegment(pdePath, kPDELineTo, pointToInsert.h, pointToInsert.v, fixedZero, fixedZero, fixedZero, fixedZero);
         }
 
-        //Insert the PDEPath insert the PDEContent that was acquired.
+        //Insert the PDEPath into the PDEContent that was acquired.
         PDEContentAddElem(pdeContent, kPDEAfterLast, reinterpret_cast<PDEElement>(pdePath));
 
-        PDPageNotifyContentsDidChange(pdPage);            //Notify the page that the PDEContent has been changed.
+        PDPageNotifyContentsDidChange(pdPage);               //Notify the page that the PDEContent has been changed.
 
-        PDPageSetPDEContentCanRaise(pdPage, 0);           //Set the PDEContent back into the PDPage's Cos object
+        PDPageSetPDEContentCanRaise(pdPage, 0);              //Set the PDEContent back into the PDPage's Cos object.
 
-        PDERelease(reinterpret_cast<PDEObject>(pdePath)); //Release the path object.
+        PDERelease(reinterpret_cast<PDEObject>(pdePath));    //Release the path object.
 
-//================================================================================================================================
-//Step 4) Create a new PDEPath object and apply some transformations to it using a transformation matrix. See Section 4.1 (Page 205) 
-//of the PDF reference for more detailed information on translations/rotations/scaling and skews.
-//================================================================================================================================
+//=============================================================================================================================================================
+// Step 4: Draw a second arrow and apply transformations. Create a new PDEPath object and apply some transformations to it using a transformation matrix. We 
+// will add a fill color of yellow to this object to differentiate between the objects.
+// Note: See Section 4.1 (Page 205) of the PDF reference for more detailed information on translations/rotations/scaling and skews.
+//=============================================================================================================================================================
 
-        PDEPath pdePath2 = PDEPathCreate();                 //Second path structure being manipulated.
+        PDEPath pdePath2 = PDEPathCreate();                       //Second path object that will be drawn to the page.
 
-        PDEPathSetPaintOp(pdePath2, kPDEStroke | kPDEFill); //Stroke and fill the object this time        
+        PDEPathSetPaintOp(pdePath2, kPDEStroke | kPDEFill);       //This arrow will be stroked and filled with color.
 
-        red = ASFloatToFixed(1.0);                          //Fill color is yellow.
+        red = ASFloatToFixed(1.0);                                //Fill color is yellow.
         blue = ASFloatToFixed(0.0);
         green = ASFloatToFixed(1.0);
     
-        //Clear out structure and fill using the new RGB values
+        //Clear out PDEColorValue struct and fill using the RBG values we just set.
         PDEColorValue fillColorValue;                               
         memset(&fillColorValue, 0, sizeof(PDEColorValue));
         fillColorValue.color[0] = red;
         fillColorValue.color[1] = green;
         fillColorValue.color[2] = blue;
-    
-        PDEColorSpec fillColorSpec{ colorSpace, fillColorValue };   //RGB Color Space, Color Value we just set.
+        
+        PDEColorSpec fillColorSpec{ colorSpace, fillColorValue };   //Fill color will also use the RGB colorspace.
 
-        gState.fillColorSpec = fillColorSpec;                       //Graphic State now contains a fill color
+        gState.fillColorSpec = fillColorSpec;                       //Graphics state now contains a fill color.
 
         PDEElementSetGState(reinterpret_cast<PDEElement>(pdePath2), &gState, sizeof(gState));
 
-        //Redraw the same arrow we just created, this will be manipulated using the transformation matrix.
+        //Redraw the exact same arrow we created in step 3.
         pointToInsert.h = ASInt32ToFixed(72 * 5);
         pointToInsert.v = ASInt32ToFixed(72 * 9);
         PDEPathAddSegment(pdePath2, kPDEMoveTo, pointToInsert.h, pointToInsert.v, fixedZero, fixedZero, fixedZero, fixedZero);
@@ -204,42 +204,41 @@ int main(int argc, char** argv)
             PDEPathAddSegment(pdePath2, kPDELineTo, pointToInsert.h, pointToInsert.v, fixedZero, fixedZero, fixedZero, fixedZero);
         }
 
-        ASDoubleMatrix translationMatrix{ 1, 0.0, 0.0, 1, (72.0 * 4.0), (-72.0 * 2.0) }; //Move 4 inches right and 2 inches down
-        ASDoubleMatrix scalingMatrix{ 0.75, 0.0, 0.0, 0.75, 0.0, 0.0 };                  //Scale to 75% of original size
-        ASDoubleMatrix translateAndScale;                                                //Holds result of last two matrices
+        ASDoubleMatrix translationMatrix{ 1, 0.0, 0.0, 1, (72.0 * 4.0), (-72.0 * 2.0) };        //Move the arrow 4 inches right and 2 inches down.
+        ASDoubleMatrix scalingMatrix{ 0.75, 0.0, 0.0, 0.75, 0.0, 0.0 };                         //Scale it to 75% of its original size.
+        ASDoubleMatrix translateAndScale;                                                       //Holds result of multiplying the last two matrices.
 
-        //IMPORTANT NOTE! If applying multiple transformations to a matrix you must concatenate
-        //the matrices before applying transformations. DO NOT attempt to call PDEElementSetMatrixEx()
-        //more than one time on any PDEElement.
+        //IMPORTANT NOTE! If applying multiple transformations to a matrix you must concatenate the matrices before applying transformations. 
+        //DO NOT attempt to call PDEElementSetMatrixEx() more than one time on the same PDEElement.
         ASDoubleMatrixConcat(&translateAndScale, &translationMatrix, &scalingMatrix);      
 
-        PDEElementSetMatrixEx(reinterpret_cast<PDEElement>(pdePath2), &translateAndScale);    //Translate and rotate the matrix.
+        PDEElementSetMatrixEx(reinterpret_cast<PDEElement>(pdePath2), &translateAndScale);       //Apply the transformation matrix.
 
-        PDEContentAddElem(pdeContent, kPDEAfterLast, reinterpret_cast<PDEElement>(pdePath2)); //Insert the PDEPath insert the PDEContent that was acquired.
+        PDEContentAddElem(pdeContent, kPDEAfterLast, reinterpret_cast<PDEElement>(pdePath2));    //Insert the PDEPath into the PDEContent that was acquired.
 
-        PDPageNotifyContentsDidChange(pdPage);  //Notify the page that the PDEContent has been changed.                                                 
+        PDPageNotifyContentsDidChange(pdPage);                                                   //Notify the page that the PDEContent has been changed.                                                 
 
-        PDPageSetPDEContentCanRaise(pdPage, 0); //Set the PDEContent back into the PDPage's Cos object
+        PDPageSetPDEContentCanRaise(pdPage, 0);                                                  //Set the PDEContent back into the PDPage's Cos object.
 
-        //Release Page, Content and Path objects.
+//=============================================================================================================================================================
+//Step 5) Release resources and save the PDF document.
+//=============================================================================================================================================================
+
+        //Release the remaining resources.
         PDERelease(reinterpret_cast<PDEObject>(colorSpace));
         PDERelease(reinterpret_cast<PDEObject>(pdePath2));
         PDPageReleasePDEContent(pdPage, 0);
         PDPageRelease(pdPage);
 
-//================================================================================================================================
-//Step 5) Save the document, catch any exceptions, ~APDFLib terminates the library.
-//================================================================================================================================
-
-        document.saveDoc(pathName, PDSaveFull | PDSaveLinearized);  //Save the document
+        document.saveDoc(pathName, PDSaveFull | PDSaveLinearized);    //Save the document
 
     HANDLER
 
         errCode = ERRORCODE;
             
-        libInit.displayError(errCode);    //Display the error code
+        libInit.displayError(errCode);                                //If there was an error, display the error that occured.
 
     END_HANDLER
 
-    return errCode;  //Return 0 if no errors
+    return errCode;                                                   //APDFLib's destructor terminates the library.
 }
