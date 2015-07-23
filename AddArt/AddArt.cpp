a6cd67744ef753b5e2cf953cb31b6cab05deb747
Jeremy Savarese <jsavarese@datalogics.com>
2015-07-23 13:45:07 -0500

    AddArt - No changes made yet. Going to update comments.

---
+++ AddArt/AddArt.cpp
@@ -0,0 +1,245 @@
+// Copyright(c) 2015, Datalogics, Inc.All rights reserved.
+
+//==============================================================================
+// Sample: AddArt, this sample demonstrates creating custom objects in
+// a PDDocument
+// Note: This program creates an output document in the working directory
+// with some basic shapes inserted into it.
+//	
+//Steps:
+// 1) Create a new PDF Document, Insert Page, Acquire PDEContent
+// 2) Set the PDEGraphicsState object containing information about how the 
+//    shape will be displayed.
+// 3) Create the shape from a list of coordinates.
+// 4) Create a copy of the shape and fill in/transform it.
+// 5) Save the PDF Document, catch any exceptions and terminate the APDFL
+//==============================================================================
+
+// This agreement is between Datalogics, Inc. 101 N.Wacker Drive, Suite 1800,
+// Chicago, IL 60606 ("Datalogics") and you, an end user who downloads
+// source code examples for integrating to the Adobe PDF Library
+// ("the Example Code"). By accepting this agreement you agree to be bound
+// by the following terms of use for the Example Code.
+//
+// LICENSE
+// -------
+// Datalogics hereby grants you a royalty - free, non - exclusive license to
+// download and use the Example Code for any lawful purpose.There is no charge
+// for use of Example Code.
+//
+// OWNERSHIP
+// ---------
+// The Example Code and any related documentation and trademarks are and shall
+// remain the sole and exclusive property of Datalogics and are protected by
+// the laws of copyright in the U.S.and other countries.
+//
+// Datalogics is a trademark of Datalogics, Inc.
+//
+// TERM
+// ----
+// This license is effective until terminated.You may terminate it at any
+// other time by destroying the Example Code.
+//
+// WARRANTY DISCLAIMER
+// -------------------
+// THE EXAMPLE CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// DATALOGICS DISCLAIM ALL OTHER WARRANTIES, CONDITIONS, UNDERTAKINGS OR
+// TERMS OF ANY KIND, EXPRESS OR IMPLIED, WRITTEN OR ORAL, BY OPERATION OF
+// LAW, ARISING BY STATUTE, COURSE OF DEALING, USAGE OF TRADE OR OTHERWISE,
+// INCLUDING, WARRANTIES OR CONDITIONS OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE, SATISFACTORY QUALITY, LACK OF VIRUSES, TITLE,
+// NON - INFRINGEMENT, ACCURACY OR COMPLETENESS OF RESPONSES, RESULTS, AND / OR
+// LACK OF WORKMANLIKE EFFORT.THE PROVISIONS OF THIS SECTION SET FORTH
+// SUBLICENSEE'S SOLE REMEDY AND DATALOGICS'S SOLE LIABILITY WITH RESPECT
+// TO THE WARRANTY SET FORTH HEREIN.NO REPRESENTATION OR OTHER AFFIRMATION
+// OF FACT, INCLUDING STATEMENTS REGARDING PERFORMANCE OF THE EXAMPLE CODE,
+// WHICH IS NOT CONTAINED IN THIS AGREEMENT, SHALL BE BINDING ON DATALOGICS.
+// NEITHER DATALOGICS WARRANT AGAINST ANY BUG, ERROR, OMISSION, DEFECT,
+// DEFICIENCY, OR NONCONFORMITY IN ANY EXAMPLE CODE. 
+
+#include <iostream>
+#include "PagePDECntCalls.h"
+#include "PEWCalls.h"
+#include "PERCalls.h"
+#include "ASCalls.h"
+#include "ASExtraCalls.h"
+#include "InitializeLibrary.h"
+#include "APDFLDoc.h"
+
+int main(int argc, char** argv)
+{
+
+    //Initialize the APDFL and check for errors
+    APDFLib libInit;
+    ASErrorCode errCode = 0;
+
+    if (libInit.isValid() == false)
+        return libInit.getInitError();
+
+//================================================================================================================================
+//Step 1) Create a new PDF Document, Insert Page, Acquire Content
+//================================================================================================================================
+    DURING
+
+        wchar_t* pathName = L"AddArtOut.pdf"; //Output file path
+
+        APDFLDoc document;  //Create a new document
+
+        document.insertPage(Int32ToFixed((72 * 10)), Int32ToFixed((72 * 10)), PDBeforeFirstPage);   //Insert a 10 inch by 10 inch page
+    
+        PDPage pdPage = document.getPage(0);                  //Retrieve the page that was just created.
+
+        PDEContent pdeContent = PDPageAcquirePDEContent(pdPage, 0); //Acquire list of contents from page
+
+        PDEPath pdePath = PDEPathCreate();                          //Create path object, which be made into a shape
+
+//================================================================================================================================
+//Step 2) Set the PDEGraphicsState attributes of the PDEPath, this structure will determine display information for the PDEPath 
+//object such as color, line width line joins, etc.
+//================================================================================================================================
+        
+        const short numberOfPoints = 7;                         //Number of points required to draw an arrow
+
+        PDEPathSetPaintOp(pdePath, kPDEStroke);                 //PDEPath will be stroked, but not filled
+
+        PDEGraphicState gState;                                 //Structure determines display information for the PDEPath
+        memset(&gState, 0, sizeof(gState));                     //Initially values will be 0
+
+        ASFixed red = ASFloatToFixed(.6);                       //Red Intensity                              
+        ASFixed green = ASFloatToFixed(.2);                     //Green Intensity
+        ASFixed blue = ASFloatToFixed(1.0);                     //Blue Intensity
+
+        PDEColorValue strokeColorValue;                         //Will be assigned to PDEGraphicState.strokeColorSpec
+
+        memset(&strokeColorValue, 0, sizeof(PDEColorValue));    //Initially value = 0 (Black)
+        strokeColorValue.color[0] = red;                        //Assign values to the struct
+        strokeColorValue.color[1] = green;                      //Values provided will show purple
+        strokeColorValue.color[2] = blue;
+
+        PDEColorSpace colorSpace = PDEColorSpaceCreateFromName(ASAtomFromString("DeviceRGB")); //RGB color space
+
+        PDEColorSpec strokeColorSpec = {colorSpace, strokeColorValue}; //Struct initializes gState.strokeColorSpec
+
+        gState.strokeColorSpec = strokeColorSpec; //Set to "DeviceRBG" and Purple
+        gState.lineWidth = Int32ToFixed(3);       //Line width is 1
+        gState.lineCap = 1;                       //Rounded line caps
+        gState.lineJoin = 1;                      //Rounded line joins
+        gState.miterLimit = fixedTen;             //Set to default value
+        gState.flatness = fixedZero;              //Set to default value
+
+        //Set the PDEPath's graphic state with the structure that was just filled out.
+        PDEElementSetGState(reinterpret_cast<PDEElement>(pdePath), &gState, sizeof(PDEGraphicState));
+
+//================================================================================================================================
+//Step 3) Create an arrow from a list of coordinates by calling PDEPathAddSegment to join the X,Y coordinates with straight lines.
+//================================================================================================================================
+
+        //(X, Y) coordinates for points on the arrow that is being generated. (72 pixels in one inch)
+        ASInt32 arrowXCoordinates[numberOfPoints] {(72 * 7), (72 * 6), (72 * 6), (72 * 4), (72 * 4), (72 * 3), (72 * 5)};
+        ASInt32 arrowYCoordinates[numberOfPoints] {(72 * 7), (72 * 7), (72 * 3), (72 * 3), (72 * 7), (72 * 7), (72 * 9)};
+        ASFixedPoint pointToInsert; 
+
+        //Move to start point at the tip of the arrow before drawing lines.
+        pointToInsert.h = ASInt32ToFixed(72 * 5);
+        pointToInsert.v = ASInt32ToFixed(72 * 9);
+        PDEPathAddSegment(pdePath, kPDEMoveTo, pointToInsert.h, pointToInsert.v, fixedZero, fixedZero, fixedZero, fixedZero);
+
+        //Iterate through the coordinates and draw lines connecting each (X,Y) pair.
+        for (unsigned i = 0; i < numberOfPoints; ++i)
+        {
+            pointToInsert.h = ASInt32ToFixed(arrowXCoordinates[i]);
+            pointToInsert.v = ASInt32ToFixed(arrowYCoordinates[i]);
+
+            PDEPathAddSegment(pdePath, kPDELineTo, pointToInsert.h, pointToInsert.v, fixedZero, fixedZero, fixedZero, fixedZero);
+        }
+
+        //Insert the PDEPath insert the PDEContent that was acquired.
+        PDEContentAddElem(pdeContent, kPDEAfterLast, reinterpret_cast<PDEElement>(pdePath));
+
+        PDPageNotifyContentsDidChange(pdPage);            //Notify the page that the PDEContent has been changed.
+
+        PDPageSetPDEContentCanRaise(pdPage, 0);           //Set the PDEContent back into the PDPage's Cos object
+
+        PDERelease(reinterpret_cast<PDEObject>(pdePath)); //Release the path object.
+
+//================================================================================================================================
+//Step 4) Create a new PDEPath object and apply some transformations to it using a transformation matrix. See Section 4.1 (Page 205) 
+//of the PDF reference for more detailed information on translations/rotations/scaling and skews.
+//================================================================================================================================
+
+        PDEPath pdePath2 = PDEPathCreate();                 //Second path structure being manipulated.
+
+        PDEPathSetPaintOp(pdePath2, kPDEStroke | kPDEFill); //Stroke and fill the object this time        
+
+        red = ASFloatToFixed(1.0);                          //Fill color is yellow.
+        blue = ASFloatToFixed(0.0);
+        green = ASFloatToFixed(1.0);
+    
+        //Clear out structure and fill using the new RGB values
+        PDEColorValue fillColorValue;                               
+        memset(&fillColorValue, 0, sizeof(PDEColorValue));
+        fillColorValue.color[0] = red;
+        fillColorValue.color[1] = green;
+        fillColorValue.color[2] = blue;
+    
+        PDEColorSpec fillColorSpec{ colorSpace, fillColorValue };   //RGB Color Space, Color Value we just set.
+
+        gState.fillColorSpec = fillColorSpec;                       //Graphic State now contains a fill color
+
+        PDEElementSetGState(reinterpret_cast<PDEElement>(pdePath2), &gState, sizeof(gState));
+
+        //Redraw the same arrow we just created, this will be manipulated using the transformation matrix.
+        pointToInsert.h = ASInt32ToFixed(72 * 5);
+        pointToInsert.v = ASInt32ToFixed(72 * 9);
+        PDEPathAddSegment(pdePath2, kPDEMoveTo, pointToInsert.h, pointToInsert.v, fixedZero, fixedZero, fixedZero, fixedZero);
+
+        for (unsigned i = 0; i < numberOfPoints; ++i)
+        {
+            pointToInsert.h = ASInt32ToFixed(arrowXCoordinates[i]);
+            pointToInsert.v = ASInt32ToFixed(arrowYCoordinates[i]);
+
+            PDEPathAddSegment(pdePath2, kPDELineTo, pointToInsert.h, pointToInsert.v, fixedZero, fixedZero, fixedZero, fixedZero);
+        }
+
+        ASDoubleMatrix translationMatrix{ 1, 0.0, 0.0, 1, (72.0 * 4.0), (-72.0 * 2.0) }; //Move 4 inches right and 2 inches down
+        ASDoubleMatrix scalingMatrix{ 0.75, 0.0, 0.0, 0.75, 0.0, 0.0 };                  //Scale to 75% of original size
+        ASDoubleMatrix translateAndScale;                                                //Holds result of last two matrices
+
+        //IMPORTANT NOTE! If applying multiple transformations to a matrix you must concatenate
+        //the matrices before applying transformations. DO NOT attempt to call PDEElementSetMatrixEx()
+        //more than one time on any PDEElement.
+        ASDoubleMatrixConcat(&translateAndScale, &translationMatrix, &scalingMatrix);      
+
+        PDEElementSetMatrixEx(reinterpret_cast<PDEElement>(pdePath2), &translateAndScale);    //Translate and rotate the matrix.
+
+        PDEContentAddElem(pdeContent, kPDEAfterLast, reinterpret_cast<PDEElement>(pdePath2)); //Insert the PDEPath insert the PDEContent that was acquired.
+
+        PDPageNotifyContentsDidChange(pdPage);  //Notify the page that the PDEContent has been changed.                                                 
+
+        PDPageSetPDEContentCanRaise(pdPage, 0); //Set the PDEContent back into the PDPage's Cos object
+
+        //Release Page, Content and Path objects.
+        PDERelease(reinterpret_cast<PDEObject>(colorSpace));
+        PDERelease(reinterpret_cast<PDEObject>(pdePath2));
+        PDPageReleasePDEContent(pdPage, 0);
+        PDPageRelease(pdPage);
+
+//================================================================================================================================
+//Step 5) Save the document, catch any exceptions, ~APDFLib terminates the library.
+//================================================================================================================================
+
+        document.saveDoc(pathName, PDSaveFull | PDSaveLinearized);  //Save the document
+
+    HANDLER
+
+        errCode = ERRORCODE;
+            
+        libInit.displayError(errCode);    //Display the error code
+
+    END_HANDLER
+
+    return errCode;  //Return 0 if no errors
+}
