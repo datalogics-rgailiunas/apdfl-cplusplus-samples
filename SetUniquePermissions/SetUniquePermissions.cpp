aea96d721e013d48a496cb9a67a03da51751b3f4
Jordan Marks <jmarks@datalogics.com>
2015-07-27 11:21:26 -0500

    Conventionalized, updated to new file dir structure.

--- SetUniquePermissions/SetUniquePermissions.cpp
+++ SetUniquePermissions/SetUniquePermissions.cpp
@@ -1,16 +1,16 @@
 // Copyright(c) 2015, Datalogics, Inc.All rights reserved.
 
-//************************************************************************
+//=======================================================================
 // Sample: SetUniquePermissions - Changes the user security permissions
 //              of the input document to allow or deny any permission
 //              you'd like.
 //
 //Steps: 
 //1) Select which permissions you want to allow/deny.
-//2) Create the security data with the supplied permissions.
-//3) Set the security data into the document.
+//2) Create new security data with the specified permissions.
+//3) Set the new security data into the document.
 //4) Save and close the document.
-//************************************************************************
+//=======================================================================
 
 // This agreement is between Datalogics, Inc. 101 N.Wacker Drive, Suite 1800,
 // Chicago, IL 60606 ("Datalogics") and you, an end user who downloads
@@ -57,170 +57,160 @@
 // NEITHER DATALOGICS WARRANT AGAINST ANY BUG, ERROR, OMISSION, DEFECT,
 // DEFICIENCY, OR NONCONFORMITY IN ANY EXAMPLE CODE.
 
-#include "ASExtraCalls.h"
-#include "ASExtraCalls.h"
-#include "PDFInit.h"
-#include "PDFLCalls.h"
-#include "PDExpT.h"
-#include "PDFInit.h"
-#include "PDFLCalls.h"
-#include "ASCalls.h"
-#include "PDCalls.h"
-#include "PERCalls.h"
-#include "PagePDECntCalls.h"
 #include "APDFLDoc.h"
 #include "InitializeLibrary.h"
 
-//To reduce verbosity
-#define B_P_PAIR std::make_pair<bool,PDPerms>
+//To reduce verbosity.
+#define PERM_PAIR std::make_pair<bool,PDPerms>
 
 int main(int argc, char** argv)
 {
+    APDFLib lib;                                                 //Initialize the Adobe PDF Library.
 
-    //Initialize the APDF Library.
-    APDFLib lib;
-    if (!lib.isValid())
-        return lib.getInitError();    //Will display the error, if any.
+    if (lib.isValid() == false)                                  //If it failed to initialize, return the error code.
+        return lib.getInitError();
 
-    //Sample variables
-    ASErrorCode errCode = 0;                                              //Tracks runtime errors in the application
-    wchar_t* inPath  = L"../Input/SetUniquePermissions.pdf";        //Input document path
-    wchar_t* outPath = L"SetUniquePermissions_Out.pdf";    //Output document path
-    char*   password = "Datalogics";                                //Password to change permissions
+    wchar_t* inPath  = L"../_Input/SetUniquePermissions.pdf";    //Input document path.
+    wchar_t* outPath = L"SetUniquePermissions_Out.pdf";          //Output document path to save the new document to.
+    char*   password = "Datalogics";                             //Password we'll add for changing permissions in the output document.
 
-//=============================================================================
-//Step 1) Select which permissions you want to allow.
-//=============================================================================
+    ASErrorCode errCode = 0;                                     //This will catch error codes thrown during library usage.
 
-    std::vector<std::pair<bool, PDPerms>> permList(17);    //There are 17 permissions here
+//====================================================================================================================================================================================================================================================================
+//Step 1) Select which permissions you want to allow/deny.
+//====================================================================================================================================================================================================================================================================
 
-    //////////////////////////////
-    ////All permissions
-    //////////////////////////////
+    std::vector<std::pair<bool, PDPerms>> permList(17);          //There are 17 permissions listed here.
+
+    ////////////////////////////////
+    ////All permissions           //
+    ////////////////////////////////
     //All permissions.
-    permList.push_back(B_P_PAIR(false, pdPermUser));
+    permList.push_back(PERM_PAIR(false, pdPermUser));
     //The user is permitted to perform all operations, regardless of the permissions specified by the document. Unless this permission is set, the document's permissions will be reset to those in the document after a full save.
-    permList.push_back(B_P_PAIR(false, pdPermOwner));
+    permList.push_back(PERM_PAIR(false, pdPermOwner));
 
-    //////////////////////////////
-    ////User Editing Permissions
-    //////////////////////////////
+    ////////////////////////////////
+    ////User Editing Permissions  //
+    ////////////////////////////////
     //The OR of all operations that can be set by the user in the security restrictions (pdPermEdit + pdPermEditNotes + pdPermPrint + pdPermCopy).
-    permList.push_back(B_P_PAIR(false, pdPermSettable));
+    permList.push_back(PERM_PAIR(false, pdPermSettable));
     //The user can edit the document more than adding or modifying text notes (see also pdPermEditNotes). In the Document Security dialog, this corresponds to the Changing the Document entry.
-    permList.push_back(B_P_PAIR(true, pdPermEdit));
+    permList.push_back(PERM_PAIR(true, pdPermEdit));
     //The user can add, modify, and delete text notes (see also pdPermEdit). In the document restrictions, this corresponds to the Authoring Comments and Form Fields entry.
-    permList.push_back(B_P_PAIR(false, pdPermEditNotes));
+    permList.push_back(PERM_PAIR(false, pdPermEditNotes));
     //Overrides various pdPermEdit bits and allows the following operations: page insert/delete/rotate and create bookmark and thumbnail.
-    permList.push_back(B_P_PAIR(false, pdPrivPermDocAssembly));
+    permList.push_back(PERM_PAIR(false, pdPrivPermDocAssembly));
     //Overrides other PDPerm bits. It allows the user to fill in or sign existing form or signature fields.
-    permList.push_back(B_P_PAIR(false, pdPrivPermFillandSign));
+    permList.push_back(PERM_PAIR(false, pdPrivPermFillandSign));
     //This should be set if the user can submit forms outside of the browser. This bit is a supplement to pdPrivPermFillandSign.
-    permList.push_back(B_P_PAIR(false, pdPrivPermFormSubmit));
+    permList.push_back(PERM_PAIR(false, pdPrivPermFormSubmit));
 
-    //////////////////////////////
-    ////Copying
-    //////////////////////////////
+    ////////////////////////////////
+    ////Copying                   //
+    ////////////////////////////////
     //The user can copy information from the document to the clipboard. In the document restrictions, this corresponds to the Content Copying or Extraction entry.
-    permList.push_back(B_P_PAIR(false, pdPermCopy));
+    permList.push_back(PERM_PAIR(false, pdPermCopy));
     //Overrides pdPermCopy to enable the Accessibility API. If a document is saved in Rev2 format (Acrobat 4.0 compatible), only the pdPermCopy bit is checked to determine the Accessibility API state.
-    permList.push_back(B_P_PAIR(false, pdPrivPermAccessible));
+    permList.push_back(PERM_PAIR(false, pdPrivPermAccessible));
 
-    //////////////////////////////
-    ////Printing
-    //////////////////////////////
+    ////////////////////////////////
+    ////Printing                  //
+    ////////////////////////////////
     //The user can print the document. Page Setup access is unaffected by this permission, since that affects Acrobat's preferences - not the document's. In the Document Security dialog, this corresponds to the Printing entry.
-    permList.push_back(B_P_PAIR(true, pdPermPrint));
+    permList.push_back(PERM_PAIR(true, pdPermPrint));
     //This bit is a supplement to pdPermPrint. If it is clear (disabled) only low quality printing (Print As Image) is allowed. On UNIX platforms where Print As Image doesn't exist, printing is disabled.
-    permList.push_back(B_P_PAIR(true, pdPrivPermHighPrint));
+    permList.push_back(PERM_PAIR(true, pdPrivPermHighPrint));
 
-    //////////////////////////////
-    ////Open/Save
-    //////////////////////////////
+    ////////////////////////////////
+    ////Open/Save                 //
+    ////////////////////////////////
     //The user can open and decrypt the document.
-    permList.push_back(B_P_PAIR(false, pdPermOpen));
+    permList.push_back(PERM_PAIR(false, pdPermOpen));
     //The user can perform a Save As.... If both pdPermEdit and pdPermEditNotes are disallowed, Save will be disabled but Save As... will be enabled. The Save As... menu item is not necessarily disabled even if the user is not permitted to perform a Save As....
-    permList.push_back(B_P_PAIR(false, pdPermSaveAs));
+    permList.push_back(PERM_PAIR(false, pdPermSaveAs));
 
-    //////////////////////////////
-    ////Security
-    //////////////////////////////
+    ////////////////////////////////
+    ////Security                  //
+    ////////////////////////////////
     //The user can change the document's security settings.
-    permList.push_back(B_P_PAIR(false, pdPermSecure));
+    permList.push_back(PERM_PAIR(false, pdPermSecure));
     //The OR of all operations that can be set by the user in the security restrictions (pdPermPrint + pdPermEdit + pdPermCopy + pdPermEditNotes).
-    permList.push_back(B_P_PAIR(false, pdPermSettable));
+    permList.push_back(PERM_PAIR(false, pdPermSettable));
 
-    //////////////////////////////
-    ////Templates
-    //////////////////////////////
+    ////////////////////////////////
+    ////Templates                 //
+    ////////////////////////////////
     //This should be set if the user can spawn template pages. This bit will allow page template spawning even if pdPermEdit and pdPermEditNotes are clear.
-    permList.push_back(B_P_PAIR(false, pdPrivPermFormSpawnTempl));
+    permList.push_back(PERM_PAIR(false, pdPrivPermFormSpawnTempl));
 
     DURING
 
-//=============================================================================
-//Step 2) Create the security data with the supplied permissions.
-//=============================================================================
-
         std::cout << "Opening the input document." << std::endl;
 
-        APDFLDoc APDoc(inPath,true);
+        APDFLDoc APDoc(inPath, true);
         PDDoc document = APDoc.getPDDoc();
+
+//====================================================================================================================================================================================================================================================================
+//Step 2) Create new security data with the specified permissions.
+//====================================================================================================================================================================================================================================================================
 
         std::cout << "Creating the new security data." << std::endl;
 
-        //This structure will hold the new security data
-        PDDocSetNewCryptHandler(document, ASAtomFromString("Standard"));   //Prepare to create new security data
-        StdSecurityData securityData = (StdSecurityData)PDDocNewSecurityData(document);    //...and create it
+        //This structure will hold the new security data.
+        PDDocSetNewCryptHandler(document, ASAtomFromString("Standard"));                   //Prepare to create new security data...
+        StdSecurityData securityData = (StdSecurityData)PDDocNewSecurityData(document);    //...and create it.
         securityData->size = sizeof(StdSecurityDataRec);
 
-        //See Samples "AddPassword" and "LockDocument" for password demonstration.
+        //Our output will have no password to open it. See Samples "AddPassword" and "LockDocument" for password demonstration.
         securityData->hasUserPW = false;
 
-        //The permissions of the document cannot be changed back unless the user supplies this password.
+        //The permissions of the document will not be able to be changed back unless the user supplies this password.
         securityData->hasOwnerPW = true;
         securityData->newOwnerPW = true;
         strcpy(securityData->ownerPW, password);
 
-        //Set the encryption method
-        //2 = CF_METHOD_RC4_V2 - RC4 algorithm
-        //5 = CF_METHOD_AES_V1 - AES algorithm with a zero initialization vector
-        //6 = CF_METHOD_AES_V2 - AES algorithm with a 16 byte random initialization vector
-        //7 = CF_METHOD_AES_V3 - AES algorithm with a 4 byte random initialization vector
+        //Set the encryption method:
+        //2 = CF_METHOD_RC4_V2 - RC4 algorithm.
+        //5 = CF_METHOD_AES_V1 - AES algorithm with a zero initialization vector.
+        //6 = CF_METHOD_AES_V2 - AES algorithm with a 16 byte random initialization vector.
+        //7 = CF_METHOD_AES_V3 - AES algorithm with a 4 byte random initialization vector.
         securityData->encryptMethod = 2;
-        securityData->keyLength = 16;    //encryption key length in bytes
+        securityData->keyLength = 16;                                                      //encryption key length in bytes.
         securityData->encryptMetadata = true;
         securityData->encryptAttachmentsOnly = false;
 
-        //Set the supplied user permissions flags
-        securityData->perms = 0x00000000;
-        for (std::pair<const bool, PDPerms> x : permList)
+        //Set the supplied user permissions flags.
+        securityData->perms = 0x00000000;                                                  //First, default to no permissions...
+        for (std::pair<const bool, PDPerms> x : permList)                                  //Then bitwise OR it with all the permissions we paired with true.
             if (x.first) securityData->perms |= x.second;
 
-        std::cout << "New security permissions have been set...." << std::endl;
+        std::cout << "New security permissions have been created...." << std::endl;
 
-//=============================================================================
-//Step 3) Set the security data into the document.
-//=============================================================================
+//====================================================================================================================================================================================================================================================================
+//Step 3) Set the new security data into the document.
+//====================================================================================================================================================================================================================================================================
 
         PDDocSetNewSecurityData(document, (void*)securityData);
         ASfree((void*)securityData);
 
         std::cout << "...and added to the document." << std::endl;
 
-//=============================================================================
+//====================================================================================================================================================================================================================================================================
 //Step 4) Save and close the document.
-//=============================================================================
+//====================================================================================================================================================================================================================================================================
 
         std::cout << "Saving the new file." << std::endl;
-        APDoc.saveDoc(outPath, PDSaveFull | PDSaveLinearized);
+        APDoc.saveDoc(outPath, PDSaveFull);                                                //Changing the security handler requires a full save. APDFLDoc defaults
+                                                                                           //   to PDSaveFull, but it's important to make this explicit.
+        std::cout << "Success." << std::endl;
 
     HANDLER
 
         errCode = ERRORCODE;
+        lib.displayError(errCode);                                                         //If there was an error, display it.
 
     END_HANDLER
 
-    if (errCode) lib.displayError(errCode);    //If there was an error, display it
-    return errCode;                            //End. lib's destructor terminates the library.
+    return errCode;                                                                        //lib's destructor terminates the library.
 };
