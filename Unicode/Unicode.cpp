b21abd0b284f59eaa08ba2c475cc02c24171fb70
Jordan Marks <jmarks@datalogics.com>
2015-08-10 17:13:37 -0500

    Places the vertical texts. Needs cleanup, possible simplification, and commenting.

--- Unicode/Unicode.cpp
+++ Unicode/Unicode.cpp
@@ -59,6 +59,8 @@
 #include "PSFCalls.h"
 #include "PERCalls.h"
 #include "PEWCalls.h"
+#include "PagePDECntCalls.h"
+#include "DLExtrasCalls.h"
 
 
 int main(int argc, char** argv)
@@ -71,18 +73,17 @@ int main(int argc, char** argv)
     ASErrorCode errCode = 0;                             //This will catch error codes thrown during library usage.
 
     //Note the lack of BOM and any other identifiers.
+    //Note also the null-termination. These are all required characteristics for
+    //creating an ASText object from a unicode string like these.
 
     //UTF8. This could have been a regular, double-quoted string.
     ASUTF8Val englishStr_U8[] = {
-        0x55, 0x6E, 0x69, 0x76, 0x65,
-        0x72, 0x73, 0x61, 0x6C, 0x20,
-        0x44, 0x65, 0x63, 0x6C, 0x61,
-        0x72, 0x61, 0x74, 0x69, 0x6F,
-        0x6E, 0x20, 0x6F, 0x66, 0x20,
-        0x48, 0x75, 0x6D, 0x61, 0x6E,
-        0x20, 0x52, 0x69, 0x67, 0x68,
-        0x74, 0x73,
-        0x00};
+        0x55, 0x6E, 0x69, 0x76, 0x65, 0x72, 0x73, 0x61, 0x6C, 0x20,
+        0x44, 0x65, 0x63, 0x6C, 0x61, 0x72, 0x61, 0x74, 0x69, 0x6F,
+        0x6E, 0x20, 0x6F, 0x66, 0x20, 0x48, 0x75, 0x6D, 0x61, 0x6E,
+        0x20, 0x52, 0x69, 0x67, 0x68, 0x74, 0x73,
+        0x00
+    };
 
     //UTF16BE
     ASUTF8Val frenchStr_U16B[] = {
@@ -100,25 +101,23 @@ int main(int argc, char** argv)
             0x00, 0x65,
             0x00, 0x00
     };
-
-    //UTF16LE
-    ASUTF8Val cyrillicStr_U16L[] = {
-            0x1f, 0x04, 0x3e, 0x04, 0x36, 0x04, 0x30, 0x04,
-            0x3b, 0x04, 0x43, 0x04, 0x39, 0x04, 0x41, 0x04,
-            0x42, 0x04, 0x30, 0x04, 0x2c, 0x00, 0x20, 0x00,
-            0x34, 0x04, 0x30, 0x04, 0x39, 0x04, 0x42, 0x04,
-            0x35, 0x04, 0x20, 0x00, 0x3d, 0x04, 0x30, 0x04,
-            0x3c, 0x04, 0x20, 0x00, 0x3f, 0x04, 0x3e, 0x04,
-            0x3b, 0x04, 0x43, 0x04, 0x40, 0x04, 0x30, 0x04,
-            0x41, 0x04, 0x3f, 0x04, 0x30, 0x04, 0x34, 0x04,
-            0x30, 0x04, 0x20, 0x00, 0x42, 0x04, 0x40, 0x04,
-            0x00, 0x38, 0x04, 0x2c, 0x00, 0x20, 0x00, 0x30,
-            0x04, 0x20, 0x00, 0x42, 0x04, 0x3e, 0x04, 0x20,
-            0x00, 0x4f, 0x04, 0x20, 0x00, 0x31, 0x04, 0x43,
-            0x04, 0x34, 0x04, 0x43, 0x04,
-            0x00, 0x00,
-    };
-
+    //UTF16LE MUST BECOME BE!
+    //ASUTF8Val cyrillicStr_U16L[] = {
+    //        0x1f, 0x04, 0x3e, 0x04, 0x36, 0x04, 0x30, 0x04,
+    //        0x3b, 0x04, 0x43, 0x04, 0x39, 0x04, 0x41, 0x04,
+    //        0x42, 0x04, 0x30, 0x04, 0x2c, 0x00, 0x20, 0x00,
+    //        0x34, 0x04, 0x30, 0x04, 0x39, 0x04, 0x42, 0x04,
+    //        0x35, 0x04, 0x20, 0x00, 0x3d, 0x04, 0x30, 0x04,
+    //        0x3c, 0x04, 0x20, 0x00, 0x3f, 0x04, 0x3e, 0x04,
+    //        0x3b, 0x04, 0x43, 0x04, 0x40, 0x04, 0x30, 0x04,
+    //        0x41, 0x04, 0x3f, 0x04, 0x30, 0x04, 0x34, 0x04,
+    //        0x30, 0x04, 0x20, 0x00, 0x42, 0x04, 0x40, 0x04,
+    //        0x00, 0x38, 0x04, 0x2c, 0x00, 0x20, 0x00, 0x30,
+    //        0x04, 0x20, 0x00, 0x42, 0x04, 0x3e, 0x04, 0x20,
+    //        0x00, 0x4f, 0x04, 0x20, 0x00, 0x31, 0x04, 0x43,
+    //        0x04, 0x34, 0x04, 0x43, 0x04,
+    //        0x00, 0x00,
+    //};
     //UTF32BE
     ASUTF8Val japaneseStr_U32B[] = {
             0x00, 0x00, 0x30, 0x0E, 0x00, 0x00, 0x4E, 0x16,
@@ -127,61 +126,53 @@ int main(int argc, char** argv)
             0x00, 0x00, 0x8A, 0x00, 0x00, 0x00, 0x30, 0x0F,
             0x00, 0x00, 0x00, 0x00,
     };
-    //UTF32LE
-    ASUTF8Val koreanStr_U32L[] = {
-        0x7C, 0xC7, 0x00, 0x00, 0x44, 0xB1, 0x00, 0x00,
-        0xD0, 0xC5, 0x00, 0x00, 0x44, 0xC5, 0x00, 0x00,
-        0x0C, 0xBE, 0x00, 0x00, 0x7C, 0xB7, 0x00, 0x00,
-        0x68, 0xD5, 0x00, 0x00, 0x58, 0xC7, 0x00, 0x00,
-        0x3C, 0xBB, 0x00, 0x00, 0x40, 0xC7, 0x00, 0x00,
-        0xC8, 0xB2, 0x00, 0x00, 0x1C, 0xC8, 0x00, 0x00,
-        0x74, 0xB9, 0x00, 0x00, 0x15, 0xAC, 0x00, 0x00,
-        0x4D, 0xD6, 0x00, 0x00, 0x18, 0xC2, 0x00, 0x00,
-        0x83, 0xAC, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
+    ASUTF8Val koreanStr_U32B[] = {
+        0x00, 0x00, 0xC7, 0x7C, 0x00, 0x00, 0xB1, 0x44,
+        0x00, 0x00, 0xC5, 0xD0, 0x00, 0x00, 0xC5, 0x44,
+        0x00, 0x00, 0xBE, 0x0C, 0x00, 0x00, 0xB7, 0x7C,
+        0x00, 0x00, 0xD5, 0x68, 0x00, 0x00, 0xC7, 0x58,
+        0x00, 0x00, 0xBB, 0x3C, 0x00, 0x00, 0xC7, 0x40,
+        0x00, 0x00, 0xB2, 0xC8, 0x00, 0x00, 0xC8, 0x1C,
+        0x00, 0x00, 0xB9, 0x74, 0x00, 0x00, 0xAC, 0x15,
+        0x00, 0x00, 0xD6, 0x4D, 0x00, 0x00, 0xC2, 0x18,
+        0x00, 0x00, 0xAC, 0x83,
+        0x00, 0x00, 0x00, 0x00
     };
 
     DURING
-
-
 
 ///////////////
 //// RETRIEVE FONTS 
 //////////////
 
-        char* euFontName = "CourierStd";             //this font will be used for English, French, and Cyrillic text.
-        char* jnFontName = "KozGoPr6N-Medium";       //This font will be used for Japanese text.
-        char* krFontName = "AdobeMyungjoStd-Medium"; //This font will be used for Korean text.
-
-        char* fontType = "Type0";                //All fonts used are Type0.
-
-        char* horizEnc = "Identity-H";           //The encoding we'll use for horizontal text.
-
         char* vertEnc  = "Identity-V";           //The encoding we'll use for vertical text.
+        char* horzEnc= "Identity-H";           //The encoding we'll use for vertical text.
 
+        PDEFontAttrs euFontAttrs;
+        char* euFontName = "CourierStd";             //this font will be used for English, French, and Cyrillic text.
+        memset(&euFontAttrs, 0, sizeof(euFontAttrs));
         PDEFont euVFont;
         PDEFont euHFont;
-        PDEFontAttrs euFontAttrs;
 
+        PDEFontAttrs jnFontAttrs;
+        char* jnFontName = "KozGoPr6N-Medium";       //This font will be used for Japanese text.
+        memset(&jnFontAttrs, 0, sizeof(jnFontAttrs));
         PDEFont jnVFont;
         PDEFont jnHFont;
-        PDEFontAttrs jnFontAttrs;
 
+        PDEFontAttrs krFontAttrs;
+        char* krFontName = "AdobeMyungjoStd-Medium"; //This font will be used for Korean text.
+        memset(&krFontAttrs, 0, sizeof(krFontAttrs));
         PDEFont krVFont;
         PDEFont krHFont;
-        PDEFontAttrs krFontAttrs;
 
-        memset(&euFontAttrs, 0, sizeof(euFontAttrs));
-        memset(&jnFontAttrs, 0, sizeof(jnFontAttrs));
-        memset(&krFontAttrs, 0, sizeof(krFontAttrs));
+        euFontAttrs.type = jnFontAttrs.type = krFontAttrs.type = ASAtomFromString("Type0");
 
         euFontAttrs.name = ASAtomFromString(euFontName);
         jnFontAttrs.name = ASAtomFromString(jnFontName);
         krFontAttrs.name = ASAtomFromString(krFontName);
 
-        euFontAttrs.type = jnFontAttrs.type = krFontAttrs.type = ASAtomFromString(fontType);
-
-        PDSysEncoding identityHEnc = PDSysEncodingCreateFromCMapName(ASAtomFromString(vertEnc));
+        PDSysEncoding identityHEnc = PDSysEncodingCreateFromCMapName(ASAtomFromString(horzEnc));
         PDSysEncoding identityVEnc = PDSysEncodingCreateFromCMapName(ASAtomFromString(vertEnc));
 
         PDEFontCreateFlags fontFlags = (PDEFontCreateFlags)
@@ -189,105 +180,125 @@ int main(int argc, char** argv)
 
         PDSysFont nextSysFont;
 
+        //these arrays are for looping that later, maybe.
+        PDEFontAttrs *reqFontAt[] { &euFontAttrs, &euFontAttrs, &jnFontAttrs, &krFontAttrs};
+        PDEFont *requiredFontsV[] { &euVFont,     &euVFont,     &jnVFont,     &krVFont};
+        PDEFont *requiredFontsH[] { &euHFont,     &euHFont,     &jnHFont,     &krHFont};
+
+
+        //Todo: loop this?
         nextSysFont = PDFindSysFont(&euFontAttrs, sizeof(PDEFontAttrs), 0);
         euVFont = PDEFontCreateFromSysFontAndEncoding(nextSysFont, identityVEnc, euFontAttrs.name, fontFlags);
-        //euHFont = PDEFontCreateFromSysFontAndEncoding(nextSysFont, identityHEnc, euFontAttrs.name, fontFlags);
-
+        euHFont = PDEFontCreateFromSysFontAndEncoding(nextSysFont, identityHEnc, euFontAttrs.name, fontFlags);
+      
         nextSysFont = PDFindSysFont(&jnFontAttrs, sizeof(PDEFontAttrs), 0);
         jnVFont = PDEFontCreateFromSysFontAndEncoding(nextSysFont, identityVEnc, jnFontAttrs.name, fontFlags);
-        //jnHFont = PDEFontCreateFromSysFontAndEncoding(nextSysFont, identityHEnc, jnFontAttrs.name, fontFlags);
+        jnHFont = PDEFontCreateFromSysFontAndEncoding(nextSysFont, identityHEnc, jnFontAttrs.name, fontFlags);
 
         nextSysFont = PDFindSysFont(&krFontAttrs, sizeof(PDEFontAttrs), 0);
         krVFont = PDEFontCreateFromSysFontAndEncoding(nextSysFont, identityVEnc, krFontAttrs.name, fontFlags);
-        //krHFont = PDEFontCreateFromSysFontAndEncoding(nextSysFont, identityHEnc, krFontAttrs.name, fontFlags);
+        krHFont = PDEFontCreateFromSysFontAndEncoding(nextSysFont, identityHEnc, krFontAttrs.name, fontFlags);
 
 ///////////////
-//// NEXT?
+//// PREPARE THE TEXT OBJETS!
+//////////////
+
+        //Looping this would be overkill.
+        ASText enAST = ASTextFromUnicode((ASUTF16Val*)englishStr_U8, kUTF8);
+        ASText frAST  = ASTextFromUnicode((ASUTF16Val*)frenchStr_U16B, kUTF16BigEndian);
+        ASText jnAST = ASTextFromUnicode((ASUTF16Val*)japaneseStr_U32B, kUTF32BigEndian);
+        ASText krAST = ASTextFromUnicode((ASUTF16Val*)koreanStr_U32B, kUTF32BigEndian);
+
+#define NUM_TEXTS 4
+
+        ASText *texts[] { &enAST,   &frAST,   &jnAST,   &krAST }; //todo: make it more obvious this corresponds to the arrays above.
+
+        ASUns32 firstBadGlyph = 0;
+
+        for (int i = 0; i < NUM_TEXTS; ++i)
+        {
+            if (!PDEFontCheckASTextIsRepresentable(*requiredFontsV[i], *texts[i], &firstBadGlyph)) //RequiredFontsV[i] and requiredFontsH[i] are just encoded differently, so should have the same glyphs.
+            {
+                PDEFontAttrs fontAtt;
+                PDEFontGetAttrs(*requiredFontsV[i], &fontAtt, sizeof(PDEFontAttrs));
+                std::wcout << L"Error: could not place " << i << "th text: " << ASAtomGetString(fontAtt.name)
+                           << " is missing the glyph at position " << firstBadGlyph << "." << std::endl;
+                return -1;
+            }
+        }
+
+
+///////////////
+//// PREPARE TO DRAW!
 //////////////
 
 
+        //Create the output document
+        APDFLDoc outDoc;
+        outDoc.insertPage(FloatToASFixed(8.5 * 72),FloatToASFixed(11 * 72),kPDEBeforeFirst); //Insert an 8.5" by 7.5" page.
+        PDPage outPage = outDoc.getPage(0);
+        PDEContent outPageCont = PDPageAcquirePDEContent(outPage, 0);
+        PDEText pageText = PDETextCreate(); //We'll fill this with the texts.
 
-        //APDFLDoc outDoc;
-        //outDoc.insertPage(8 * 72, 11 * 72,kPDEBeforeFirst); //Insert an 8.5" by 7.5" page.
-        //PDPage outPage = outDoc.getPage(0);
-        //PDEContent outPageCont = PDPageAcquirePDEContent(outPage, 0);
-
-
-
-
-
-        //see orig smp
-        PDEColorSpace ColorSpace = PDEColorSpaceCreateFromName(ASAtomFromString("DeviceGray"));
-        memset(ColorSpace, 0, sizeof(PDEGraphicState));
-        PDEGraphicState gState;
-        gState.strokeColorSpec.space = gState.fillColorSpec.space = ColorSpace;
-        gState.miterLimit = fixedTen;
-        gState.flatness = fixedOne;
-        gState.lineWidth = fixedOne;
-
-        //PREPARE FOR VERTICAL PLACEMENT
+        //PREPARE FOR GENERAL TEXT PLACMENT
+        //Prepare matrices
         ASFixedRect pageRect;
-        //PDPageGetCropBox(outPage, &pageRect);
-
+        PDPageGetCropBox(outPage, &pageRect);
         ASFixedMatrix strPlaceMatrix;
-        strPlaceMatrix.a = Int16ToFixed(14); //Font width/height
-        strPlaceMatrix.d = Int16ToFixed(18); //18-pt size...
-        strPlaceMatrix.h = Int16ToFixed(1 * 72); //x,y coord on page.
-        //strPlaceMatrix.v = pageRect.top - Int16ToFixed(1 * 72); //Begin 1 inch from top of page.
+        memset(&strPlaceMatrix, 0, sizeof(strPlaceMatrix));
+        strPlaceMatrix.a = Int16ToFixed(20); //Font width, in points.
+        strPlaceMatrix.d = Int16ToFixed(14); //Font height, in points.
+        strPlaceMatrix.h = Int16ToFixed(2 * 72); //X coord on page. Two inches from left side.
+        strPlaceMatrix.v = pageRect.top - Int16ToFixed(1 * 72); //Y coord on page.One inch from the top.
 
-        //ASText engAST = ASTextNew();
-        //engAST = ASTextFromUnicode((ASUTF16Val*)englishStr_U8, kUTF8);
-        //std::cout << "yeay" << std::endl;
+        //Prepare graphics state
+        PDEColorSpace colorSpace = PDEColorSpaceCreateFromName(ASAtomFromString("DeviceGray"));
+        PDEGraphicState gS;
+        memset(&gS, 0, sizeof(PDEGraphicState));
+        gS.strokeColorSpec.space = gS.fillColorSpec.space = colorSpace;
+        bool isV = true;
 
-        /////I think it is likely that non utf-8 needs to be in two-byte strings.
+        for (int i = 0; i < NUM_TEXTS; ++i)
+        {
+            PDEFont** nextFont = (isV ? requiredFontsV : requiredFontsH);
+            PDETextAddASText(pageText, kPDETextRun, i, *texts[i], *nextFont[i],&gS, sizeof(gS), NULL, 0, &strPlaceMatrix);
 
-        //ASText frAST  = ASTextFromUnicode((ASUTF16Val*)frenchStr_U16B, kUTF16BigEndian);
-        //std::cout << "yeay" << std::endl;
-        //ASText jnAST = ASTextFromUnicode((ASUTF16Val*)japaneseStr_U32B, kUTF32BigEndian);
-        //std::cout << "yeay" << std::endl;
-        //
-        //ASUns32 firstBadGlyph;
-        //if (PDEFontCheckASTextIsRepresentable(euVFont, engAST, &firstBadGlyph))
-        //{
-        //}
-        //else
-        //{
-        //    std::cout << "uh oh, brother! E" << std::endl;
-        //}
-        //ASUns32 ThirstBadGlyph;
-        //if (PDEFontCheckASTextIsRepresentable(euVFont, frAST, &ThirstBadGlyph))
-        //{
-        //}
-        //else
-        //{
-        //    std::cout << "uh oh, brother! f" << std::endl;
-        //}
-        //ASUns32 thirstBadGlyph;
-        //if (PDEFontCheckASTextIsRepresentable(jnVFont, jnAST, &thirstBadGlyph))
-        //{
-        //}
-        //else
-        //{
-        //    std::cout << "uh oh, brother! J" << std::endl;
-        //}
+            if (isV)
+                strPlaceMatrix.h += strPlaceMatrix.a + Int16ToFixed(10); //Advance the x value by the width of the font, plus 10 points padding for ease of readability.
+            else
+                strPlaceMatrix.v += strPlaceMatrix.d + Int16ToFixed(10); //Advance the y value by the height of the font, plus padding.
 
+        }
+        PDEContentAddElem(outPageCont, kPDEBeforeFirst, (PDEElement) pageText);
+        PDPageSetPDEContentCanRaise(outPage, 0);
 
+        //Simply calling "SubsetNow" subsets the font, creates a ToUnicode table, and creates widths.
+        CosDoc cosDoc = PDDocGetCosDoc(outDoc.getPDDoc());
+        for (int i = 0; i < NUM_TEXTS; ++i)
+        {
+            PDEFontSubsetNow(*requiredFontsV[i], cosDoc);
+            //TODO: add to subset the H fonts whenn you use them.
+        }
+        
+        PDERelease((PDEObject)pageText);
+        PDPageReleasePDEContent(outPage, 0);
+        PDPageRelease(outPage);
+        ASTextDestroy(enAST);
+        ASTextDestroy(frAST);
+        ASTextDestroy(jnAST);
         PDERelease((PDEObject)euVFont);
         PDERelease((PDEObject)jnVFont);
         PDERelease((PDEObject)krVFont);
-       // PDERelease((PDEObject)euHFont);
-       // PDERelease((PDEObject)jnHFont);
-       // PDERelease((PDEObject)krHFont);
-        PDERelease((PDEObject)ColorSpace);
-        //ASTextDestroy(engAST);
-        //ASTextDestroy(jnAST);
-        //ASTextDestroy(frAST);
 
-        //PDPageReleasePDEContent(outPage, 0);
-        //PDPageRelease(outPage);
+        //N: this actually won't work, since you have the eu font twice to start. array the fonts each text uses? is that overkill?
+        //for (int i = 0; i < NUM_FONTS; ++i)
+        //{
+        //    PDERelease((PDEObject)*requiredFontsV[i]);
+        //    //todo: add H, when you use them.
+        //}
+        PDERelease((PDEObject)colorSpace);
 
-
-
+        outDoc.saveDoc(L"Unicode.pdf");
 
     HANDLER
 
