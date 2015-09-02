e6205b8dfa126b398b2df03cdfc7c1a7a414e437
Jordan Marks <jmarks@datalogics.com>
2015-09-02 15:02:01 -0500

    RGB, not APDFL!!t:

--- CreateTransparency/CreateTransparency.cpp
+++ CreateTransparency/CreateTransparency.cpp
@@ -5,7 +5,7 @@
 //============================================================================================
 // Sample: CreateTransparency - This sample demonstrates transparency and blending modes
 //             in APDFL for CMYK and RGB color spaces. It creates 2 color blending
-//             cirles, one for APDFL and one for CMYK, once for each of the 12
+//             cirles, one for RGB and one for CMYK, once for each of the 12
 //             blending modes, and one additional set to demonstrate the absence of blending.
 //             A total of 24 color blending circles are drawn.
 //
