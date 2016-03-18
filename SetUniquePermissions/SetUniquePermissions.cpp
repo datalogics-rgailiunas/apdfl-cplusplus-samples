9d502f73d845e029e87fb0694c50c49691bc0ca3
Yijing Luo <yluo@datalogics.com>
2016-03-18 11:12:03 -0500

    Adding following samples - ExtractDocumentInfo - ExtractText - FindImageResolutions - FlattenAnnotations - FlattenTransparency - LockDocument - MergeDocuments - OpenEncrypted - RasterizeCopy - SetUniquePermissions - SplitPDF - TextSearch - UnicodeText - WebOptimizedPDF

--- SetUniquePermissions/SetUniquePermissions.cpp
+++ SetUniquePermissions/SetUniquePermissions.cpp
@@ -131,9 +131,11 @@ int main(int argc, char** argv)
 
         //Set the supplied user permissions flags.
         securityData->perms = 0x00000000;                                                  //First, default to no permissions...
-        for (std::pair<const bool, PDPerms> x : permList)                                  //Then bitwise OR it with all the permissions we paired with true.
+        for (int i = 0; i < 15; i++)                                  //Then bitwise OR it with all the permissions we paired with true.
+        {
+            std::pair<const bool, PDPerms> x = permList[i];
             if (x.first) securityData->perms |= x.second;
-
+        }
         std::cout << "New security permissions have been created...." << std::endl;
 
 //====================================================================================================================================================================================================================================================================
