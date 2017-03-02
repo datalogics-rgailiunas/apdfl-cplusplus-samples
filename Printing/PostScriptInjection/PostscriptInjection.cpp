f5607c95607924a8f4c18b0ac7df15c9c3ed294a
Mark Dawson <mdawson@datalogics.com>
2017-03-02 15:28:53 -0600

    Update PostscriptInjection.cpp

--- Printing/PostScriptInjection/PostscriptInjection.cpp
+++ Printing/PostScriptInjection/PostscriptInjection.cpp
@@ -12,8 +12,8 @@
 // is defined and available, the program will send the output to the printer, to create
 // a paper copy.
 //
-// For more detail see the description of the PostScriptInjection sample program on our Developer’s site,
-// dev.datalogics.com/adobe-pdf-library/sample-program-descriptions/c1samples#postscriptinjection.
+// For more detail see the description of the PostScriptInjection sample program on our Developerâ€™s site,
+// dev.datalogics.com/adobe-pdf-library/sample-program-descriptions/c1samples#postscriptinjection
 
 #include <cstdio>
 
