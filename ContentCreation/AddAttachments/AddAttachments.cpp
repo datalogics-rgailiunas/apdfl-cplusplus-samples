677b6d997c7aea4e3442cd0960c67e37d83b7727
Mark Dawson <mdawson@datalogics.com>
2017-03-15 13:39:33 -0500

    Update AddAttachments.cpp

--- ContentCreation/AddAttachments/AddAttachments.cpp
+++ ContentCreation/AddAttachments/AddAttachments.cpp
@@ -6,7 +6,9 @@
 //
 // This sample program adds two attachments to an input PDF document.
 //
-// For more detail see the description of the AddAttachments sample program on our Developer’s site, 
+// Command-line arguments:  <input-file> <output-file> <attachment-1> <attachment-2>   (All optional)
+//
+// For more detail see the description of the AddAttachments sample program on our Developerâ€™s site, 
 // http://dev.datalogics.com/adobe-pdf-library/sample-program-descriptions/c1samples#addattachments
 
 #include <iostream>
