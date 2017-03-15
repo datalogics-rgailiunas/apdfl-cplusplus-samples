f498a02495ccf837a60530026decd27081f92d50
Mark Dawson <mdawson@datalogics.com>
2017-03-15 13:46:44 -0500

    Update AddPageNumbers.cpp

--- ContentModification/AddPageNumbers/AddPageNumbers.cpp
+++ ContentModification/AddPageNumbers/AddPageNumbers.cpp
@@ -7,7 +7,9 @@
 // AddPageNumbers adds labels to the page numbers in a PDF document. These labels appear under the 
 // page thumbnails.
 //
-// For more detail see the description of the AddPageNumbers sample program on our Developer’s site, 
+//  Command-line:   <input-file>  <output-file>     (Both optional)
+//
+// For more detail see the description of the AddPageNumbers sample program on our Developerâ€™s site, 
 // http://dev.datalogics.com/adobe-pdf-library/sample-program-descriptions/c1samples#addpagenumbers
 
 #include "InitializeLibrary.h"
