e4b4c50785e92ae095fd65f1083256f5fc4f36de
JArgento <josepha@datalogics.com>
2017-04-19 16:41:22 -0400

    Check for error after building samples.

--- All/build_run_all.bat
+++ All/build_run_all.bat
@@ -150,6 +150,8 @@ ECHO #Building Datalogics samples...
 devenv %ALL_DL_SLN% /rebuild "%STAGE%|%ARCH%"
 ECHO.
 
+IF %ERRORLEVEL% NEQ 0 GOTO End
+
 REM *************************************************
 REM *** 3) Decide which samples to run.
 REM *************************************************
