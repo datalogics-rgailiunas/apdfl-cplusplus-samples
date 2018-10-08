75ccf81208aeca065ce0b8b1eb7231c7f91c91a7
Michael McKeough <mjm@datalogics.com>
2018-10-08 15:48:56 -0500

    Correct name in mac run script.

--- All/build_run_all_mac.sh
+++ All/build_run_all_mac.sh
@@ -117,7 +117,7 @@ declare -a DL_SAMPLE_LIST=( \
   "Security/ValidateSignatures" \
   "Text/InsertHeadFoot" \
   "Text/ExtractText" \
-  "text/TextSelectEnum" \
+  "Text/TextSelectEnum" \
   "Text/AddText" \
   "Text/UnicodeText" \
   "Text/HelloJapan" \
