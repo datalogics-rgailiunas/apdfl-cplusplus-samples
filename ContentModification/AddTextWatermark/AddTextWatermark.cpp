90bc2c61211cdcc6445accef930e13b1dacb440f
Michael McKeough <mjm@datalogics.com>
2020-03-17 12:01:31 -0500

    Rename from AddDirectWatermark  to AddTextWatermark.
    Signed-off-by: Michael McKeough <mjm@datalogics.com>

--- ContentModification/AddTextWatermark/AddTextWatermark.cpp
+++ ContentModification/AddTextWatermark/AddTextWatermark.cpp
@@ -18,8 +18,8 @@
 //
 // Command-line:   <input-file-name> <output-file-name> <Watermark text string> <font name> <Text Color> <Text Opacity> <Stroke Color> <Stroke Opacity> (All optional)
 //
-// For more detail see the description of the AddWatermark sample program on our Developer’s site,
-// http://dev.datalogics.com/adobe-pdf-library/sample-program-descriptions/c1samples#addDirectwatermark
+// For more detail see the description of the AddTextWatermark sample program on our Developer’s site,
+// http://dev.datalogics.com/adobe-pdf-library/sample-program-descriptions/c1samples#AddTextWatermark
 
 
 
