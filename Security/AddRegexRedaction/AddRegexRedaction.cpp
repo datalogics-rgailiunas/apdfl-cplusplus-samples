7e2be110d365f874b3671b69eeaf7d1a34a43b1b
Sahara Yousuf <saharay@datalogics.com>
2021-03-16 14:48:46 -0500

    Fix typo in comment

--- Security/AddRegexRedaction/AddRegexRedaction.cpp
+++ Security/AddRegexRedaction/AddRegexRedaction.cpp
@@ -113,7 +113,7 @@ int main(int argc, char **argv) {
             redactParams->textColor->value[0] = fixedOne;
             redactParams->textFontName = "CourierStd"; // Draw the overlay text using CourierStd font.
             redactParams->textSize = FloatToASFixed(10.0); // Draw the text size as 10 point (nominally, may
-                                                           // be reduced is ScaleToFit is true).
+                                                           // be reduced if ScaleToFit is true).
             redactParams->repeat = true; // Draw the text in each quad, repeating to fill the quad.
             redactParams->scaleToFit = true; // Reduce text size if needed, to fit the quad.
 
