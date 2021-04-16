8083c08a46276c3623565c035ffed28062edeec2
bruceh <bruceh@datalogics.com>
2021-04-16 14:00:30 -0500

    correct number of samples to run

--- All/build_run_all.bat
+++ All/build_run_all.bat
@@ -156,7 +156,7 @@ REM *** 3) Decide which samples to run.
 REM *************************************************
 
 REM *** The total number of samples. This must be accurate!
-SET /A "NUM_SAMPLES=55"
+SET /A "NUM_SAMPLES=57"
 
 REM *** Datalogics Samples.
 SET "DL_SAMPLE_LIST=("
@@ -224,7 +224,7 @@ SET "DL_SAMPLE_LIST=%DL_SAMPLE_LIST% InformationExtraction\ExtractDocumentInfo"
 SET "DL_SAMPLE_LIST=%DL_SAMPLE_LIST% FileSystem\AlternateFileSystem"
 SET "DL_SAMPLE_LIST=%DL_SAMPLE_LIST%)"
 REM *** The total number of DL samples. This must be accurate!
-SET /A "NUM_DL_SAMPLES=62"
+SET /A "NUM_DL_SAMPLES=60"
 
 REM *** Di iterates over Datalogics samples. Do not change this value.
 SET /A "Di=0"
