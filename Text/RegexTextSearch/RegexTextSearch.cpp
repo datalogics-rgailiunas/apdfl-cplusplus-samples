c85ba6d0f4c74e65ebeb7771077d93f138c3f372
bruceh <bruceh@datalogics.com>
2021-04-16 11:28:21 -0500

    wfConfig.noHyphenDetection = true; Need this set for specific documents like the constitution and searching for this constitution, like devtest PDFDocTextFinder_APDFL-3751 or it will not capture all matches spanning multiple lines

--- Text/RegexTextSearch/RegexTextSearch.cpp
+++ Text/RegexTextSearch/RegexTextSearch.cpp
@@ -67,12 +67,13 @@ int main(int argc, char *argv[]) {
 
         APDFLDoc document(csInputFileName.c_str(), true);
 
-        // Step 1) Set up the word finder configuration to use the default settings.
-
+        // Step 1) Setup the word finder configuration.
         PDWordFinderConfigRec wfConfig;
-
         memset(&wfConfig, 0, sizeof(wfConfig));           // Always do this!
         wfConfig.recSize = sizeof(PDWordFinderConfigRec); //...and this!
+
+        // Need to set this to true so phrases will be concatenated properly
+        wfConfig.noHyphenDetection = true;
 
         // Step 2) Fill in color information for highlighting text. In this case, our color will be set to orange.
 
