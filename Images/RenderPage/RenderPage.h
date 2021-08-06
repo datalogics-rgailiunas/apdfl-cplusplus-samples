2b1772bfff6945ae1507c5cdc2cdd07984044ca4
Joseph Argento <josepha@datalogics.com>
2021-08-06 12:32:14 -0500

    Update sample to illustrate special colorspace outputs.
    Add support for DeviceRGBA and DeviceCMYKA which are known to PDFL for supporting RGBA and CMYKA outputs respectively.
    
    Update copyright through dates and clean up sample a bit.

--- Images/RenderPage/RenderPage.h
+++ Images/RenderPage/RenderPage.h
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2017, Datalogics, Inc. All rights reserved.
+// Copyright (c) 2017-2021, Datalogics, Inc. All rights reserved.
 //
 // For complete copyright information, refer to:
 // http://dev.datalogics.com/adobe-pdf-library/license-for-downloaded-pdf-samples/
@@ -44,6 +44,8 @@ class RenderPage {
     ASInt32 SetBPC(ASInt32 bitsPerComp);
 
     static ASAtom sDeviceRGB_K;
+    static ASAtom sDeviceRGBA_K;
+    static ASAtom sDeviceCMYKA_K;
     static ASAtom sDeviceCMYK_K;
     static ASAtom sDeviceGray_K;
 
